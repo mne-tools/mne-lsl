11d2b13d633b8206c1512e76d383759e48e43184
Kyuhwa Lee <lee.kyuh@gmail.com>
2016-10-28 14:56:57 +0200

    sample scripts config update

--- Protocols/scripts_sample/config/config_test_mi.py
+++ Protocols/scripts_sample/config/config_test_mi.py
@@ -21,25 +21,27 @@ T_GAP= 4 # intertrial gap
 T_READY= 2 # only cross is shown without any direction cue
 T_DIR_CUE= 2 # direction cue shown
 T_CLASSIFY= 5 # imagery period
+T_FEEDBACK= 1 # decision feedback shown
+REFRESH_RATE= 30 # maximum refresh rate in Hz
 
 # evidence accumulation parameter
 PROB_ACC_ALPHA= 0.8 # p_new= p_old * alpha + p_new * (1-alpha)
 
-# bar bias: None or (direction, pixels)
-BAR_BIAS= ('U',1)
+# bar bias: None or (direction, probability)
+BAR_BIAS= None # ('U',0.01)
 
 # bar speed: multiplied to raw likelihoods
-BAR_STEP= 10
+BAR_STEP= 20
+
+# finish the trial if bar reaches any end
+BAR_REACH_FINISH= False
+
+# positive feedback only?
+POSITIVE_FEEDBACK= False
 
 # screen property
 SCREEN_SIZE= (1680, 1050)
 SCREEN_POS= (1920, 0)
-
-# move only when correct?
-POSITIVE_FEEDBACK= False
-
-# finish the trial if the bar reached the end?
-BAR_REACH_FINISH= False
 
 # fake decoder?
 FAKE_CLS= None # None or 'left' or 'right' or 'middle' or 'random'
@@ -55,6 +57,3 @@ GLASS_USE= False
 
 # debug likelihoods
 DEBUG_PROBS= False
-
-# maximum refresh rate in Hz
-REFRESH_RATE= 30
