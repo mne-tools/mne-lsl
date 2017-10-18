52eb1f4ce3e82edd9a53515dd3cd0ff8bcf7822e
Kyuhwa Lee <lee.kyuh@gmail.com>
2017-10-18 09:40:06 +0200

    More scripts added

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     long_description=open('README.txt').read(),
     packages=find_packages(),
     install_requires=['mne>=0.14', 'scipy', 'numpy', 'opencv-python>=3.3',\
-        'future', 'pyqtgraph'],
+        'future', 'pyqtgraph', 'matplotlib>=2.1.0'],
 )
