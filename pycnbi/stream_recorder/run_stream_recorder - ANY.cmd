1bf131e669e5c7d0e6bfca69789dc0f4e0786ac8
Kyuhwa Lee <lee.kyuh@gmail.com>
2017-10-18 09:40:06 +0200

    More scripts added

--- pycnbi/stream_recorder/run_stream_recorder - ANY.cmd
+++
@@ -1,2 +0,0 @@
-ipython stream_recorder.py
-pause
--- scripts/pycnbi_tfr_export.cmd
+++ scripts/pycnbi_tfr_export.cmd
@@ -1,3 +1,6 @@
 @ECHO OFF
-SET EXE="import pycnbi.analysis.tfr_export as m; m.config_run('%1')"
-ipython -c %EXE%
+IF NOT "%1" == "" (
+    python -c "if __name__ == '__main__': import pycnbi.analysis.tfr_export as m; m.config_run('%1')"
+) ELSE (
+    ECHO Usage: %0 [CONFIG_FILE]
+)
