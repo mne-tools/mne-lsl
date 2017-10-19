96d2a3954bdebac45148d4e0c64a3dd1a31b9c03
Kyuhwa Lee <lee.kyuh@gmail.com>
2017-10-19 09:33:08 +0200

    feature importance analysis added.

--- pycnbi/utils/pycnbi_utils.py
+++ pycnbi/utils/pycnbi_utils.py
@@ -11,20 +11,6 @@ Try raw.as_data_frame(scalings=dict(eeg=1.0, misc=1.0))
 Kyuhwa Lee, 2014
 Swiss Federal Institute of Technology Lausanne (EPFL)
 
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 
 import os
