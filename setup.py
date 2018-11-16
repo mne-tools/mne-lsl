89af76b7c09d0c9eb8aaa506384d825840ca6254
Kyuhwa Lee <lee.kyuh@gmail.com>
2018-11-16 10:25:23 +0100

    Stream Recorder better compatibility with different Python versions. setup.py readme file typo fix.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import find_packages, setup
-import os
 
 setup(
     name='pycnbi',
-    version='0.5dev',
+    version='0.6dev',
     author='Kyuhwa Lee',
     author_email='lee.kyuh@gmail.com',
     license='The GNU General Public License',
-    url='https://c4science.ch/source/pycnbi/',
+    url='https://github.com/dbdq/pycnbi/',
     description='Real-time brain-machine interface',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     packages=find_packages(),
-    install_requires=['mne>=0.14', 'scipy', 'numpy', 'opencv-python>=3.3',\
-        'future', 'pyqtgraph', 'matplotlib>=2.1.0'],
+    install_requires=['mne>=0.14', 'scipy', 'numpy', 'pylsl',\
+        'opencv-python>=3.3', 'future', 'pyqtgraph', 'matplotlib>=2.1.0',\
+        'configparser', 'psutil'],
 )
