bd7ce6d53f6538e18e4abf0dce91d7118d95c8bc
Kyuhwa Lee <lee.kyuh@gmail.com>
2019-03-14 12:57:59 +0100

    Scripts update to reflect unified function names.

--- scripts/pycnbi_train_mi.cmd
+++ scripts/pycnbi_train_mi.cmd
@@ -1,6 +1,6 @@
 @echo off
 if not "%1" == "" (
-    python -c "if __name__ == '__main__': import pycnbi.protocols.train_mi as m; m.config_run('%1')"
+    python -c "if __name__ == '__main__': import pycnbi.protocols.train_mi as m; m.batch_run('%1')"
     pause
 ) else (
     echo Usage: %0 {config_file}
