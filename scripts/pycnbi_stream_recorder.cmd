6b84b206013e81bd4424601dec6ead53cca88020
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-04-02 19:22:16 +0200

    fixed pycnbi batch scripts issues with backslash

--- scripts/pycnbi_stream_recorder.cmd
+++ scripts/pycnbi_stream_recorder.cmd
@@ -1,2 +1,2 @@
 @echo off
-python -c "if __name__ == '__main__': import pycnbi.stream_recorder.stream_recorder as m; m.batch_run('%1', '%2', '%3')"
+python -c "if __name__ == '__main__': import pycnbi.stream_recorder.stream_recorder as m; m.batch_run(r'%1', r'%2', r'%3')"
