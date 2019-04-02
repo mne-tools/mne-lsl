ffc6dc461f6b95fce9ab4617b0ebe66e7279f484
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-04-02 19:22:16 +0200

    fixed pycnbi batch scripts issues with backslash

--- scripts/pycnbi_add_lsl_events.cmd
+++ scripts/pycnbi_add_lsl_events.cmd
@@ -1,6 +1,6 @@
 @echo off
 if not "%1" == "" (
-    python -c "if __name__ == '__main__': import pycnbi.utils.add_lsl_events as m; m.add_lsl_events('%1')"
+    python -c "if __name__ == '__main__': import pycnbi.utils.add_lsl_events as m; m.add_lsl_events(r'%1')"
     pause
 ) else (
     echo Usage: %0 {path_to_event_files}
--- scripts/pycnbi_convert2fif.cmd
+++ scripts/pycnbi_convert2fif.cmd
@@ -1,6 +1,6 @@
 @echo off
 if not "%1" == "" (
-    python -c "if __name__ == '__main__': from pycnbi.utils.convert2fif import main; main('%1')"
+    python -c "if __name__ == '__main__': from pycnbi.utils.convert2fif import main; main(r'%1')"
 ) else (
     echo Usage: %0 {fif_path}
 )
