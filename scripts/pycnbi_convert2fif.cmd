5a512be17c4439e3a991699d041dc0d7978cd46f
Kyuhwa Lee <lee.kyuh@gmail.com>
2019-05-01 13:49:26 +0200

    add_lsl_events script: option to take offset argument

--- scripts/pycnbi_convert2fif.cmd
+++ scripts/pycnbi_convert2fif.cmd
@@ -2,5 +2,5 @@
 if not "%1" == "" (
     python -c "if __name__ == '__main__': from pycnbi.utils.convert2fif import main; main(r'%1')"
 ) else (
-    echo Usage: %0 {fif_path}
+    echo Usage: %0 {data path}
 )
