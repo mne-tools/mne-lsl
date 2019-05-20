bd4289444f0bc5770656c664e994a82b0abf761d
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-05-20 15:48:56 +0200

    Move classifier params order

--- pycnbi/config_files/config_structure_trainer_mi.py
+++ pycnbi/config_files/config_structure_trainer_mi.py
@@ -99,9 +99,9 @@ class Advanced:
 
     params6 = dict()
     params6.update({'CLASSIFIER': ('GB', 'XGB', 'RF', 'rLDA', 'LDA')})
-    params6.update({'EXPORT_CLS': (False, True)})
     params6.update({'GB': dict(trees=int, learning_rate=float, max_depth=int, seed=int)})
     params6.update({'RF': dict(trees=int, max_depth=int, seed=int)})
+    params6.update({'EXPORT_CLS': (False, True)})
     params6.update({'RLDA_REGULARIZE_COEFF': float})
 
     #-------------------------------------------
