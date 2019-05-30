6d20e1fcefcf0377dfb127dc174d0abe77631bc7
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-05-30 13:04:34 +0200

    Update the config_files/config_structure_test_mi.py for GUI

--- pycnbi/config_files/config_structure_test_mi.py
+++ pycnbi/config_files/config_structure_test_mi.py
@@ -3,40 +3,46 @@ class Basic:
     """
     Contains the basic parameters for the online modality of Motor Imagery protocol
     """  
-
-    # Data
+    #-------------------------------------------
+    # Decoder
+    #-------------------------------------------
     params1 = dict()
     params1.update({'DECODER_FILE': str})
-    #params1.update({'DIRECTIONS': [ ('L', 'LEFT_GO'), ('R', 'RIGHT_GO') ]})
+    params1.update({'DIRECTIONS': ('L', 'R', 'U', 'D', 'B')})
 
-    params1.update({'DIRECTIONS': (None, 'L', 'R', 'U', 'D', 'B')})
-
-
+    #-------------------------------------------
     # feedback type
+    #-------------------------------------------
     params2 = dict()
     params2.update({'FEEDBACK_TYPE': ('BAR', 'BODY')})
     params2.update({'FEEDBACK_IMAGE_PATH': str})
     params2.update({'REFRESH_RATE': int}) # Maximum refresh rate in Hz
 
-     # Trials
+    #-------------------------------------------
+    # Trials
+    #-------------------------------------------
     params3 = dict()
     params3.update({'TRIALS_EACH': int})
     params3.update({'TRIALS_RANDOMIZE': [False, True]})
     params3.update({'TRIALS_RETRY': [False, True]})
 
+    #-------------------------------------------
     # Bar behavior
+    #-------------------------------------------
     params3 = dict()
     params3.update({'PROB_ALPHA_NEW': float})
-    params3.update({'BAR_BIAS': ('L', 0.05)})
+    params3.update({'BAR_BIAS': None})
     params3.update({'BAR_STEP_LEFT': int})
     params3.update({'BAR_STEP_RIGHT': int})
     params3.update({'BAR_STEP_UP': int})
     params3.update({'BAR_STEP_DOWN': int})
-    params3.update({'BAR_SLOW_START': int})
+    params3.update({'BAR_SLOW_START': int})             # BAR_SLOW_START: None or in seconds
     params3.update({'BAR_REACH_FINISH': (False, True)})
+    params3.update({'POSTIVE_FEEDBACK': (False, True)})
 
- 
+    #-------------------------------------------
     # Screen property
+    #-------------------------------------------
     params4 = dict()
     params4.update({'SCREEN_SIZE': ([1920, 1080], [1600, 1200], [1680, 1050], [1280, 1024], [1024, 768])})
     params4.update({'SCREEN_POS': ([1920, 0], [1920, 1080])})
@@ -48,17 +54,23 @@ class Advanced:
     Contains the advanced parameters for the training modality of Motor Imagery protocol
     """
    
+    #-------------------------------------------
     # Trigger device type
+    #-------------------------------------------
     params1 = dict()
     params1.update({'TRIGGER_DEVICE': (None, 'ARDUINO','USB2LPT','SOFTWARE','DESKTOP')})
     params1.update({'TRIGGER_FILE': str}) # full list: PYCNBI_ROOT/Triggers/triggerdef_*.py
 
+    #-------------------------------------------
     # acquisition device (set both to None to search)
+    #-------------------------------------------
     params2 = dict()
     params2.update({'AMP_NAME': str})
     params2.update({'AMP_SERIAL': str})
 
+    #-------------------------------------------
     # Timings
+    #-------------------------------------------
     params3 = dict()
     params3.update({'T_INIT': int})                  # initial waiting time
     params3.update({'T_GAP': int})                   # intertrial gap
@@ -69,3 +81,25 @@ class Advanced:
     params3.update({'SHOW_CUE': (False, True)})
     params3.update({'SHOW_RESULT': (False, True)})   # show the classification result
     params3.update({'SHOW_TRIALS': (False, True)})
+
+
+    #-------------------------------------------
+    # Google Glass
+    #-------------------------------------------
+    params3 = dict()
+    params3.update({'GLASS_USE': (False, True)})
+
+    #-------------------------------------------
+    # Debug
+    #-------------------------------------------
+    params3.update({'DEBUG_PROBS': (False, True)})    
+    params3.update({'LOG_PROBS': (False, True)})
+
+    #-------------------------------------------
+    # Parallel decoding
+    #-------------------------------------------
+    params3.update({'PARALLEL_DECODING': (None, dict(period=0.06, num_strides=3))})
+
+
+
+    
