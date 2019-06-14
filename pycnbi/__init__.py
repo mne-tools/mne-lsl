4c33a61cf611a3e2264758bbed9a2dd98ac2ab4b
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-06-14 13:30:29 +0200

    Add stream as input of init_logger for multiprocess
    Allows more than one stream handle by the logger

--- pycnbi/__init__.py
+++ pycnbi/__init__.py
@@ -1,11 +1,8 @@
 '''
 Initialize PyCNBI logger and other settings
-
 TODO: support a process-safe file logging
-
 Kyuhwa Lee
 Swiss Federal Institute of Technology Lausanne (EPFL)
-
 '''
 
 import os
@@ -52,43 +49,43 @@ class PycnbiFormatter(logging.Formatter):
         self._style = logging.PercentStyle(self._fmt)
         return logging.Formatter.format(self, record)
 
-def init_logger(streamHandler, verbose_console='INFO', verbose_file=None):
-    if not logger.hasHandlers():
-        # add custom log levels
-        logging.addLevelName(LOG_LEVELS['INFO_GREEN'], 'INFO_GREEN')
-        def __log_info_green(self, message, *args, **kwargs):
-            if self.isEnabledFor(LOG_LEVELS['INFO_GREEN']):
-                self._log(LOG_LEVELS['INFO_GREEN'], message, args, **kwargs)
-        logging.Logger.info_green = __log_info_green
+def init_logger(stream, verbose_console='INFO', verbose_file=None):
+    #if not logger.hasHandlers():
+    # add custom log levels
+    logging.addLevelName(LOG_LEVELS['INFO_GREEN'], 'INFO_GREEN')
+    def __log_info_green(self, message, *args, **kwargs):
+        if self.isEnabledFor(LOG_LEVELS['INFO_GREEN']):
+            self._log(LOG_LEVELS['INFO_GREEN'], message, args, **kwargs)
+    logging.Logger.info_green = __log_info_green
 
-        logging.addLevelName(LOG_LEVELS['INFO_BLUE'], 'INFO_BLUE')
-        def __log_info_blue(self, message, *args, **kwargs):
-            if self.isEnabledFor(LOG_LEVELS['INFO_BLUE']):
-                self._log(LOG_LEVELS['INFO_BLUE'], message, args, **kwargs)
-        logging.Logger.info_blue = __log_info_blue
+    logging.addLevelName(LOG_LEVELS['INFO_BLUE'], 'INFO_BLUE')
+    def __log_info_blue(self, message, *args, **kwargs):
+        if self.isEnabledFor(LOG_LEVELS['INFO_BLUE']):
+            self._log(LOG_LEVELS['INFO_BLUE'], message, args, **kwargs)
+    logging.Logger.info_blue = __log_info_blue
 
-        logging.addLevelName(LOG_LEVELS['INFO_YELLOW'], 'INFO_YELLOW')
-        def __log_info_yellow(self, message, *args, **kwargs):
-            if self.isEnabledFor(LOG_LEVELS['INFO_YELLOW']):
-                self._log(LOG_LEVELS['INFO_YELLOW'], message, args, **kwargs)
-        logging.Logger.info_yellow = __log_info_yellow
+    logging.addLevelName(LOG_LEVELS['INFO_YELLOW'], 'INFO_YELLOW')
+    def __log_info_yellow(self, message, *args, **kwargs):
+        if self.isEnabledFor(LOG_LEVELS['INFO_YELLOW']):
+            self._log(LOG_LEVELS['INFO_YELLOW'], message, args, **kwargs)
+    logging.Logger.info_yellow = __log_info_yellow
 
-        # console logger handler
-        c_handler = logging.StreamHandler(streamHandler)
-        c_handler.setFormatter(PycnbiFormatter())
-        logger.addHandler(c_handler)
+    # console logger handler
+    c_handler = logging.StreamHandler(stream)
+    c_handler.setFormatter(PycnbiFormatter())
+    logger.addHandler(c_handler)
 
-        '''
-        # file logger handler
-        f_handler = logging.FileHandler('pycnbi.log', mode='a')
-        f_handler.setLevel(loglevels[verbose_file])
-        f_format = logging.Formatter('%(levelname)s %(asctime)s %(funcName)s:%(lineno)d: %(message)s')
-        f_handler.setFormatter(f_format)
-        logger.addHandler(f_handler)
-        '''
+    '''
+    # file logger handler
+    f_handler = logging.FileHandler('pycnbi.log', mode='a')
+    f_handler.setLevel(loglevels[verbose_file])
+    f_format = logging.Formatter('%(levelname)s %(asctime)s %(funcName)s:%(lineno)d: %(message)s')
+    f_handler.setFormatter(f_format)
+    logger.addHandler(f_handler)
+    '''
 
-        # minimum possible level of all handlers
-        logger.setLevel(logging.DEBUG)
+    # minimum possible level of all handlers
+    logger.setLevel(logging.DEBUG)
 
     set_log_level(verbose_console, verbose_file)
     return logger
@@ -111,3 +108,4 @@ for d in qc.get_dir_list(ROOT):
 logging.getLogger('matplotlib').setLevel(logging.ERROR)
 logger = logging.getLogger('pycnbi')
 logger.propagate = False
+init_logger(sys.stdout)
