e0960c6e10936a28c278a1ddbeb44539a110e868
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-07-25 17:34:17 +0200

    Copy the config file if missing in the subject folder

---
+++ pycnbi/config_files/config_train_mi.py
@@ -0,0 +1,25 @@
+TRIGGER_DEVICE = 'SOFTWARE'
+TRIGGER_FILE = r'C:\Users\adesvachez\git\pycnbi\pycnbi\triggers\triggerdef_16.ini'
+
+FEEDBACK_TYPE = 'BAR'
+FEEDBACK_IMAGE_PATH = None
+
+SCREEN_SIZE = (1920, 1080)
+SCREEN_POS = (0, 0)
+REFRESH_RATE = 50
+
+DIRECTIONS = ['L', 'R']
+DIR_RANDOM = False
+
+TRIALS_EACH = 15
+TRIAL_PAUSE = False
+
+TIMINGS = { 'INIT':2,           \
+            'GAP': 3,           \
+            'CUE': 1,            \
+            'READY': 1,         \
+            'READY_RANDOMIZE':0,    \
+            'DIR': 5,           \
+            'DIR_RANDOMIZE': 0}   
+
+GLASS_USE = False
