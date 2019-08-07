f4275441c0dec9769a74ec2f5580f730add56c10
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-08-07 15:46:25 +0200

    Automatic start and stop of the data recording.

--- pycnbi/utils/cnbi_lsl.py
+++ pycnbi/utils/cnbi_lsl.py
@@ -21,10 +21,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-
 import sys
+import time
 import pylsl
-import pycnbi.utils.q_common as qc
 from pycnbi import logger
 
 
@@ -66,7 +65,7 @@ def start_server(server_name, n_channels=1, channel_format='string', nominal_sra
     return pylsl.StreamOutlet(sinfo)
 
 
-def start_client(server_name):
+def start_client(server_name, state):
     """
     Search and connect to an LSL server
 
@@ -74,6 +73,8 @@ def start_client(server_name):
     ------
     server_name:
         Name of the server to search
+    state:
+        Multiprocessing.Value used to stop from the GUI, 1: acquire 2:stop
 
     Returns
     -------
@@ -82,6 +83,8 @@ def start_client(server_name):
 
     """
     while True:
+        if not state.value:
+            sys.exit(-1) 
         logger.info('Searching for LSL server %s ...' % server_name)
         streamInfos = pylsl.resolve_byprop("name", server_name, timeout=1)
         if not streamInfos:
