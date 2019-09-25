74fda3d739d2ce87aac7e544b78b16f67811aefd
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-09-25 12:07:31 +0200

    Change scripts' names

---
+++ scripts/pycnbi_offline_mi.cmd
@@ -0,0 +1,7 @@
+@echo off
+if not "%1" == "" (
+    python -c "if __name__ == '__main__': import pycnbi.protocols.mi.train_mi as m; m.batch_run(r'%1')"
+    pause
+) else (
+    echo Usage: %0 {config_file}
+)
