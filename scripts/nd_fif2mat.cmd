602f4543a7a19ce89695acd35062b784f9603596
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-10-08 14:53:45 +0200

    Correct scripts to new software name

--- scripts/nd_fif2mat.cmd
+++ scripts/nd_fif2mat.cmd
@@ -1,6 +1,6 @@
 @echo off
 if not "%1" == "" (
-    python -c "if __name__ == '__main__': from pycnbi.utils.fif2mat import fif2mat; fif2mat(r'%1')"
+    python -c "if __name__ == '__main__': from neurodecode.utils.fif2mat import fif2mat; fif2mat(r'%1')"
 ) else (
     echo Usage: %0 {fif_path}
 )
--- scripts/nd_stream_recorder.cmd
+++ scripts/nd_stream_recorder.cmd
@@ -1,2 +1,2 @@
 @echo off
-python -c "if __name__ == '__main__': import pycnbi.stream_recorder.stream_recorder as m; m.batch_run(r'%1', r'%2', r'%3')"
+python -c "if __name__ == '__main__': import neurodecode.stream_recorder.stream_recorder as m; m.batch_run(r'%1', r'%2', r'%3')"
