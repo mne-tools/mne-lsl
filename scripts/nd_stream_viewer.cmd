e09db7b5cb53a2448224beae02633305591dbef2
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-10-08 14:53:45 +0200

    Correct scripts to new software name

--- scripts/nd_stream_viewer.cmd
+++ scripts/nd_stream_viewer.cmd
@@ -1,4 +1,4 @@
 @echo off
-cd /D %NeuroD_ROOT%/pycnbi/stream_viewer/
+cd /D %NeuroD_ROOT%/neurodecode/stream_viewer/
 python stream_viewer.py
 pause
--- scripts/nd_tfr_export_all_signals.cmd
+++ scripts/nd_tfr_export_all_signals.cmd
@@ -1,6 +1,6 @@
 @echo off
 if not "%1" == "" (
-    python -c "if __name__ == '__main__': import pycnbi.analysis.tfr_export_all_signals as m; m.config_run(r'%1')"
+    python -c "if __name__ == '__main__': import neurodecode.analysis.tfr_export_all_signals as m; m.config_run(r'%1')"
     pause
 ) else (
     echo Usage: %0 {config_file}
