86e2b35cf3ac9cc1fa0f4f7cb513d84ca603b836
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-10-08 14:53:45 +0200

    Correct scripts to new software name

--- scripts/nd_feature_stats.cmd
+++ scripts/nd_feature_stats.cmd
@@ -1,2 +1,2 @@
 @echo off
-python -c "if __name__ == '__main__': import pycnbi.analysis.parse_features as m; m.config_run(r'%1')"
+python -c "if __name__ == '__main__': import neurodecode.analysis.parse_features as m; m.config_run(r'%1')"
