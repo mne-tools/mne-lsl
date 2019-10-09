d00f422eed2006a4442467e48c2d3281a500185b
Arnaud Desvachez <arnad.desvachez@gmail.com>
2019-10-09 11:05:16 +0200

    Modify the mi config templates and structures

--- neurodecode/config_files/mi/structure_files/config_structure_offline.py
+++ neurodecode/config_files/mi/structure_files/config_structure_offline.py
@@ -3,6 +3,12 @@ class Basic:
     """
     Contains the basic parameters for the offline modality of Motor Imagery protocol
     """  
+
+    #-------------------------------------------
+    # PATH TO SAVE DATA
+    #-------------------------------------------
+    params0 = dict()
+    params0.update({'DATA_PATH': str})
     
     #-------------------------------------------
     # Bar directions 
@@ -26,12 +32,6 @@ class Basic:
     params3.update({'TRIALS_EACH': int})               # Trial numbers for each class
     params3.update({'TRIAL_PAUSE': (False, True)})      # Pause after each trial
     
-    #-------------------------------------------
-    # Screen property
-    #-------------------------------------------
-    params4 = dict()
-    params4.update({'SCREEN_SIZE': ((1920, 1080), (1600, 1200), (1680, 1050), (1280, 1024), (1024, 768))})
-    params4.update({'SCREEN_POS': ((0, 0), (1920, 0))}) # TO CHANGE: add a check box second monitor and auto display on it. 
 
 
 ########################################################################
@@ -46,22 +46,23 @@ class Advanced:
     params1 = dict()
     params1.update({'TRIGGER_DEVICE': (None, 'ARDUINO','USB2LPT','SOFTWARE','DESKTOP')})
     params1.update({'TRIGGER_FILE': str})          # full list: PYCNBI_ROOT/Triggers/triggerdef_*.py
-    
-    #-------------------------------------------
-    # PATH TO SAVE DATA
-    #-------------------------------------------
-    params2 = dict()
-    params2.update({'DATA_PATH': str})
 
     #-------------------------------------------
     # Timings
     #-------------------------------------------
-    params3 = dict()
-    params3.update({'TIMINGS':dict(INIT=float, GAP=float, CUE=float, READY=float, READY_RANDOMIZE=float, DIR=float, DIR_RANDOMIZE=float)})
+    params2 = dict()
+    params2.update({'TIMINGS':dict(INIT=float, GAP=float, CUE=float, READY=float, READY_RANDOMIZE=float, DIR=float, DIR_RANDOMIZE=float)})
 
     #-------------------------------------------
     # Google Glass
     #-------------------------------------------
+    params3 = dict()
+    params3.update({'GLASS_USE': (False, True)})
+
+    #-------------------------------------------
+    # Screen property
+    #-------------------------------------------
     params4 = dict()
-    params4.update({'GLASS_USE': (False, True)})
+    params4.update({'SCREEN_SIZE': ((1920, 1080), (1600, 1200), (1680, 1050), (1280, 1024), (1024, 768))})
+    params4.update({'SCREEN_POS': ((0, 0), (1920, 0))}) # TO CHANGE: add a check box second monitor and auto display on it. 
     
