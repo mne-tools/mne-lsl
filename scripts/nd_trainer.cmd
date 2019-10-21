ebc9175b086207f51217da7a8e9345bcbae6ad55
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2019-10-21 16:43:27 +0200

    Move trainer in protocols + rename the protocols

--- scripts/nd_trainer.cmd
+++ scripts/nd_trainer.cmd
@@ -1,6 +1,6 @@
 @echo off
 if not "%1" == "" (
-    python -c "if __name__ == '__main__': import neurodecode.decoder.trainer as m; m.batch_run(r'%1')"
+    python -c "if __name__ == '__main__': import neurodecode.protocols.mi.trainer as m; m.batch_run(r'%1')"
     pause
 ) else (
     echo Usage: %0 {config_file}
