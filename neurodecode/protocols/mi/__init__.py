21a11bbd47fa7ee8e9cf974db7b9b675821f8f94
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2020-05-05 13:51:03 +0200

    Add doc to the __init__.py for modules documentation

---
+++ neurodecode/protocols/mi/__init__.py
@@ -0,0 +1,8 @@
+'''
+Motor imagery protocol
+
+Contains the three protocols to perform a motor imagery BCI task:
+    - offline_mi    =   data acquisition while the subject is performing mi tasks
+    - trainer_mi    =   classifier training on the previously acquired data
+    - online_mi     =   real-time classification of the subject's mi
+'''
