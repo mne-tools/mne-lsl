5cb34d609fd7872978ebdcdb09f9e78ba747cb3f
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2020-11-14 12:02:48 +0100

    Add adaptive classifier option to mi

--- neurodecode/config_files/mi/template_files/config_online.py
+++ neurodecode/config_files/mi/template_files/config_online.py
@@ -7,6 +7,7 @@ DECODER_FILE = r''
 # fake decoder?
 FAKE_CLS = None
 DIRECTIONS = [ ('L', 'LEFT_GO'), ('R', 'RIGHT_GO') ]
+ADAPTIVE =  {'selected':'False', 'False':False, 'True':[3]}
 
 # trigger device
 TRIGGER_DEVICE = None # None | 'ARDUINO' | 'USB2LPT' | 'DESKTOP' | 'SOFTWARE'
