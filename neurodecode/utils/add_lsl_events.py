e215037bb1620d83446ca70e146877e245f5ed60
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-03-29 15:45:23 +0200

    Refactor add_lsl_events

--- neurodecode/utils/add_lsl_events.py
+++ neurodecode/utils/add_lsl_events.py
@@ -1,40 +1,26 @@
 from __future__ import print_function, division
-
 from neurodecode import logger
 
-
-def add_lsl_events(event_dir, offset=0, recursive=False, interactive=True):
+#----------------------------------------------------------------------
+def add_lsl_events(event_dir, recursive=False, interactive=True):
     """
-    Add events recorded with LSL timestamps to raw data files.
-    Useful for software triggering.
+    Add LSL events to raw data. Data must be pcl file to be converted to fif format.
+    
+    Useful for software trigger. The data in pickle and the event in txt must be in the same folder.
 
-    @params
-    -------
-    event_dir:
-    Path to *-eve.txt files.
-
-    offset:
-    Timestamp offset (in seconds) in case the LSL server timestamps are shifted.
-    Some OpenVibe acquisition servers send timestamps of their own running time (always
-    starting from 0) instead of LSL timestamps. In this case, the only way to deal with
-    this problem is to add an offset, a difference between LSL timestamp and OpenVibe
-    server time stamp.
-
-    recursive:
-    Search sub-directories recursively.
-
-
-    Kyuhwa Lee
-    Swiss Federal Institute of Technology Lausanne (EPFL)
-    2017
+    Parameters
+    ----------
+    event_dir : str
+        Path to the folder containing both *-eve.txt and *-raw.pcl
+    recursive : bool
+        Search sub-directories recursively
+    interactive : bool
+        If true, press Enter will be asked to convert the found files
     """
     import neurodecode.utils.q_common as qc
     from neurodecode.utils.convert2fif import pcl2fif
     from builtins import input
 
-    offset = float(offset)
-    if offset != 0:
-        logger.info_yellow('Time offset = %.3f' % offset)
     to_process = []
     logger.info('Files to be processed')
     if recursive:
@@ -53,10 +39,16 @@ def add_lsl_events(event_dir, offset=0, recursive=False, interactive=True):
         input('\nPress Enter to start')
     for f in to_process:
         pclfile = f.replace('-eve.txt', '-raw.pcl')
-        pcl2fif(pclfile, external_event=f, offset=offset, overwrite=True)
+        pcl2fif(pclfile, external_event=f)
 
-# sample code
+#----------------------------------------------------------------------
 if __name__ == '__main__':
-    event_dir = r'D:\data\Records'
-    offset = -0.093936
-    add_lsl_events(event_dir, offset=offset, recursive=False)
+    
+    from pathlib import Path
+    
+    event_dir = str(Path(input(">> Provide the path to the folder containing the events files: \n")))
+    
+    recursive = input(">> Do you search recursively (subfolders)? (y / n) \n")
+    recursive = True if recursive in ['y', 'yes', 'Y', 'YES', 'Yes'] else False
+    
+    add_lsl_events(event_dir, recursive=recursive, interactive=True)
