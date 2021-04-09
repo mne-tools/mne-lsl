bd4897b00eec332d0deb53e105629cf3d5359f8a
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-04-09 14:14:09 +0200

    Fix refactor bug

--- neurodecode/utils/convert/__init__.py
+++ neurodecode/utils/convert/__init__.py
@@ -4,5 +4,5 @@ Format conversion.
 It supports converting from .xdf | .gdf | .bdf | .eeg | .pickle files to mne.io.raw and from mne.io.raw to .mat.
 '''
 
-from .convert2fif import any2fif, pcl2fif, edf2fif, bdf2fif, gdf2fif, xdf2fif, eeg2fif, event_timestamps_to_indices, add_events_from_txt
+from .convert2fif import any2fif, pcl2fif, edf2fif, bdf2fif, gdf2fif, xdf2fif, eeg2fif
 from .fif2mat import fif2mat
