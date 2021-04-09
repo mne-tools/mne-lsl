1927c4438b1177ebfbd1945e501d04d8e72a7fe8
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-04-09 14:23:17 +0200

    Add bulletpoints to doc

--- neurodecode/utils/convert/__init__.py
+++ neurodecode/utils/convert/__init__.py
@@ -1,7 +1,10 @@
 '''
 Format conversion.
 
-It supports converting from .xdf | .gdf | .bdf | .eeg | .pickle files to mne.io.raw and from mne.io.raw to .mat.
+It supports converting:
+
+- from .xdf | .gdf | .bdf | .eeg | .pickle files to mne.io.raw
+- from mne.io.raw to .mat.
 '''
 
 from .convert2fif import any2fif, pcl2fif, edf2fif, bdf2fif, gdf2fif, xdf2fif, eeg2fif
