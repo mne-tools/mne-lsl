f432d845adedf750f4ee0c335969503ec9028fef
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-04-09 10:50:52 +0200

    Refactor fif2mat

--- neurodecode/utils/convert/__init__.py
+++ neurodecode/utils/convert/__init__.py
@@ -5,3 +5,4 @@ It supports converting from .xdf | .gdf | .bdf | .eeg | .pickle files to mne.io.
 '''
 
 from .convert2fif import any2fif, pcl2fif, edf2fif, bdf2fif, gdf2fif, xdf2fif, eeg2fif, event_timestamps_to_indices, add_events_from_txt
+from .fif2mat import fif2mat
