8782603c8c75bf36d405afb175c5aa8ba8078167
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-04-13 11:42:19 +0200

    Add benchmark module

---
+++ neurodecode/utils/benchmark/__init__.py
@@ -0,0 +1,8 @@
+"""
+Benchmark testing
+
+Module for assessing performance.
+"""
+
+from .benchmark_decoder import benchmark_BCIdecoder, BCIDecoderDaemon
+from .benchmark_multitaper import benchmark_multitaper
