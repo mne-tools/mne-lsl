bae91cfdc95b97ba36d8b645ac432b3930e91bd5
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-04-14 09:46:59 +0200

    Move LSL functions to proper folder

--- neurodecode/utils/lsl/__init__.py
+++ neurodecode/utils/lsl/__init__.py
@@ -1,5 +1,5 @@
 """
-LSL wrapper functions for creating a server and a client
+Functions for interacting with LSL network (start server or client...)
 """
 
-from .lsl import  start_server, start_client
+from .lsl import  start_server, start_client, list_lsl_streams, search_lsl, lsl_channel_list
