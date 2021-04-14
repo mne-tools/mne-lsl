8ee2a26b40969da58a6c25e7dbcbe8ba250dad1f
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-04-14 09:46:59 +0200

    Move LSL functions to proper folder

--- neurodecode/utils/lsl/lsl.py
+++ neurodecode/utils/lsl/lsl.py
@@ -8,7 +8,7 @@ import multiprocessing as mp
 from neurodecode import logger
 
 #----------------------------------------------------------------------
-def start_server(server_name, n_channels=1, channel_format='string', nominal_srate=pylsl.IRREGULAR_RATE, stype='EEG',
+def start_server(server_name, n_channels=1, channel_format='string', nominal_srate=pylsl.IRREGULAR_RATE, stype='Markers',
                  source_id=None):
     """
     Start a new LSL server.
@@ -43,14 +43,14 @@ def start_server(server_name, n_channels=1, channel_format='string', nominal_sra
 #----------------------------------------------------------------------
 def start_client(server_name, state=mp.Value('i', 1)):
     """
-    Search and connect to an LSL server.
+    Search for an LSL outlet (server) and open an LSL inlet (client).
 
     Parameters
     ----------
     server_name: str
         Name of the server to search
     state : Multiprocessing.Value 
-        Used to searching stop from another process.
+        Used to stop searching from another process.
 
     Returns
     -------
@@ -70,3 +70,116 @@ def start_client(server_name, state=mp.Value('i', 1)):
         break
     
     return pylsl.StreamInlet(sinfo)
+
+#----------------------------------------------------------------------
+def list_lsl_streams(ignore_markers=False, logger=logger, state=mp.Value('i', 1)):
+    """
+    List all the available outlets on LSL network.
+    
+    Parameters
+    ----------
+    ignore_markers : bool
+        If True, ignore streams with Marker type
+    logger : logging.Logger
+        The logger to output info
+    state: mp.Value
+        The multiprocess sharing variable, used to stop search from another process
+    """
+    import time
+
+    # look for LSL servers
+    amp_list = []
+    amp_list_backup = []
+
+    while state.value == 1:
+        
+        streamInfos = pylsl.resolve_streams()
+        
+        if len(streamInfos) > 0:
+            
+            for index, si in enumerate(streamInfos):
+                amp_name = si.name()
+                if 'Markers' in si.type():
+                    amp_list_backup.append((index, amp_name))
+                else:
+                    amp_list.append((index, amp_name))
+            break
+        
+        logger.info('No server available yet on the network...')
+        time.sleep(1)
+
+    if ignore_markers is False:
+        amp_list += amp_list_backup
+
+    logger.info('-- List of servers --')
+    
+    for i, (index, amp_name) in enumerate(amp_list):
+        logger.info('%d: %s' % (i, amp_name))
+
+    return amp_list, streamInfos
+
+#----------------------------------------------------------------------
+def search_lsl(ignore_markers=False, logger=logger, state=mp.Value('i', 1)):
+    """
+    Search and select an available stream on LSL network
+    
+    Does not open a LSL inlet.
+    
+    Parameters
+    ----------
+    ignore_markers : bool
+        If True, ignore streams with Marker type
+    logger : logging.Logger
+        The logger to output info
+    state: mp.Value
+        The multiprocess sharing variable, used to stop search from another process
+    
+    Returns:
+    --------
+    str : The selected amp name
+    """
+    amp_list, streamInfos = list_lsl_streams(ignore_markers, logger, state)
+
+    if len(amp_list) == 1:
+        index = 0
+    else:
+        index = input('Amp index? Hit enter without index to select the first server.\n>> ')
+        if index.strip() == '':
+            index = 0
+        else:
+            index = int(index.strip())
+    
+    amp_index, amp_name = amp_list[index]
+    si = streamInfos[amp_index]
+    assert amp_name == si.name()
+    
+    logger.info('Selected: %s' % (amp_name))
+
+    return amp_name
+
+#----------------------------------------------------------------------
+def lsl_channel_list(inlet):
+    """
+    Reads the XML description of the LSL header and returns the channels name list.
+
+    Parameters
+    ----------
+    inlet : pylsl.StreamInlet
+        The inlet to extract channels list
+        
+    Returns:
+    --------
+        list : List of channels name [ name1, name2, ... ]
+    """
+    if not type(inlet) is pylsl.StreamInlet:
+        logger.error('Wrong input type %s' % type(inlet))
+        raise TypeError
+
+    ch = inlet.info().desc().child('channels').first_child()
+    ch_list = []
+    for k in range(inlet.info().channel_count()):
+        ch_name = ch.child_value('label')
+        ch_list.append(ch_name)
+        ch = ch.next_sibling()
+    
+    return ch_list
