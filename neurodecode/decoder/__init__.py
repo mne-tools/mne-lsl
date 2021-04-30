c7eaca7319f68c5a80e1371ac0b4e847c375db73
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-04-30 17:53:29 +0200

    Refactor decoder module

--- neurodecode/decoder/__init__.py
+++ neurodecode/decoder/__init__.py
@@ -1,8 +1,9 @@
 '''
-Module providing a decoder for BCI and neurofeedback experiments
+Module providing a decoder for BCI and neurofeedback experiments.
  
 Currently, LDA, regularized LDA, Random Forests, and Gradient Boosting Machines 
 are supported as classifier types.
 '''
 
 from .decoder import BCIDecoder, BCIDecoderDaemon, get_decoder_info,  check_speed, sample_decoding, log_decoding
+from .features import compute_features, feature2chz
