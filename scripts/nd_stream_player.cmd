147942ed5fb2af0370f7d8606af041b1567d5ee6
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-05-05 11:16:00 +0200

    Update nd_stream_player

--- scripts/nd_stream_player.cmd
+++ scripts/nd_stream_player.cmd
@@ -1,16 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
 
-if not "%1" == "" (
-	set fif=%1
-	set fif=!fif:\=/!
-	if "%2" == "" (
-		set chunk=16
-	) else (
-		set chunk=%2
-	)
-    python -c "if __name__ == '__main__': from neurodecode.stream_player import Streamer; sp = Streamer('StreamPlayer', '!fif!', !chunk!); sp.stream()"
-    pause
-) else (
-    echo Usage: %0 {fif_file} [chunk_size=16]
-)
+python %NEUROD_ROOT%/neurodecode/stream_player/stream_player.py %1 %2 %3
+
