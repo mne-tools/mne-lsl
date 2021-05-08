f966281d7a880cb20daf3079084fe95a38682b18
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-05-08 12:11:57 +0200

    Add new functions

--- neurodecode/utils/etc/__init__.py
+++ neurodecode/utils/etc/__init__.py
@@ -1,6 +1,7 @@
 '''
-Module containing usefull functions for dict-list objects and bit conversion
+Module containing usefull functions, mainly for dict-list objects and bit conversion
 '''
 
-from .list_dict_related import list2string,  flatten_list,  get_index_min,  get_index_max, sort_by_value,  detect_delim
+from .images2pkl import images2pkl
 from .bit_conversion import int2bits, bits2int
+from .list_dict_related import list2string,  flatten_list,  get_index_min,  get_index_max, sort_by_value,  detect_delim
--- neurodecode/utils/io/__init__.py
+++ neurodecode/utils/io/__init__.py
@@ -10,8 +10,12 @@ For converting, it supports:
 
 from .mat2fif import mat2fif
 from .fif2mat import fif2mat
+from .fif_info import fif_info
 from .load_mat import load_mat
 from .load_config import load_config
+from .fif_resample import fif_resample
+from .merge_events import merge_events
+from .fix_channel_names import fix_channel_names
 from .load_fif import load_fif_raw, load_fif_multi
 from .convert2fif import any2fif, pcl2fif, edf2fif, bdf2fif, gdf2fif, xdf2fif, eeg2fif
 from .io_file_dir import get_file_list, get_dir_list, make_dirs, save_obj, load_obj, loadtxt_fast, parse_path, forward_slashify
