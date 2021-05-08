a6f897ca2e830bfb3df083430c32519989914fec
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-05-08 16:39:26 +0200

    Update all windows cmd scripts

--- scripts/nd_stream_recorder.cmd
+++ scripts/nd_stream_recorder.cmd
@@ -1,2 +1,4 @@
 @echo off
-python %NEUROD_ROOT%/neurodecode/stream_recorder/stream_recorder.py
+setlocal enabledelayedexpansion
+
+python %NEUROD_ROOT%/neurodecode/stream_recorder/stream_recorder.py %1 %2
