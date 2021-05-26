5499a43e45136985e12d2cd65095833ee99e335e
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-05-26 19:23:52 +0200

    Cleanup in utils.io

--- neurodecode/utils/io/__init__.py
+++ neurodecode/utils/io/__init__.py
@@ -8,11 +8,9 @@ For converting, it supports:
 
 '''
 
-from .mat2fif import mat2fif
-from .fif2mat import fif2mat
-from .load_mat import load_mat
-from .load_config import load_config
-from .merge_events import merge_events
-from .read_raw_fif import read_raw_fif, read_raw_fif_multi
 from .convert2fif import any2fif, pcl2fif, edf2fif, bdf2fif, gdf2fif, xdf2fif, eeg2fif, event_timestamps_to_indices
+from .export import write_set, dir_write_set
 from .io_file_dir import get_file_list, get_dir_list, make_dirs
+from .load_config import load_config
+from .load_mat import load_mat
+from .read_raw_fif import read_raw_fif, read_raw_fif_multi
