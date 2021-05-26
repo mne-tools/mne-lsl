2a3a0092c93b0897d02d7e5f5a09223bdc2aef73
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-05-26 19:29:45 +0200

    Reworked load_config.py.

--- neurodecode/utils/io/load_config.py
+++ neurodecode/utils/io/load_config.py
@@ -1,25 +1,27 @@
-import os
 import sys
-
-from importlib import import_module
 from pathlib import Path
+from importlib import import_module
 
-#----------------------------------------------------------------------
+from ... import logger
+
+
 def load_config(cfg_module):
     """
     Dynamic loading of a config file module.
-    
+
     Parameters
     ----------
     cfg_module : str
-        The absolute path to the config file to load 
+        The absolute path to the config file to load.
     """
-    if not os.path.isabs(cfg_module):
-        cfg_module = os.path.abspath(cfg_module)
-        if not os.path.isfile(cfg_module):
-            raise Exception('Provide the absolute path to the config file to load.')
-    
     cfg_module = Path(cfg_module)
+    if not cfg_module.is_abolute():
+        cfg_module = cfg_module.absolute()
+        if not cfg_module.exists():
+            logger.error('CFG file not found. '
+                         'Provide the absolute path to the config file to load.')
+            raise IOError
+
     sys.path.append(str(cfg_module.parent))
-    
+
     return import_module(cfg_module.stem)
