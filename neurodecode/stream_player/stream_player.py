70d558c3a2ba6cb25d35b47b54ffaaf03a8661fe
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-05-27 00:08:20 +0200

    Applied PEP8, modified and changed to relative imports.

--- neurodecode/stream_player/stream_player.py
+++ neurodecode/stream_player/stream_player.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 
+"""
+Strean a recorded fif file on LSL network.
+
+Command-line arguments:
+    #1: Server name
+    #2: Raw FIF file to stream
+    #3: Chunk size
+    #4: Trigger file
+"""
+
 import time
 import pylsl
 import numpy as np
 import multiprocessing as mp
 
-from neurodecode import logger
-from neurodecode.triggers import TriggerDef
-from neurodecode.utils.io import load_fif_raw
-from neurodecode.utils.preprocess import find_event_channel
+from .. import logger
+from ..triggers import TriggerDef
+from ..utils.io import read_raw_fif
+from ..utils.events import find_event_channel
 
 
 class StreamPlayer:
     """
-    Class for playing recorded file on LSL network in another process.
+    Class for playing a recorded file on LSL network in another process.
 
     Parameters
     ----------
@@ -58,12 +68,15 @@ class StreamPlayer:
             resolution. However, it uses more CPU.
         """
         self._process = mp.Process(target=self._stream,
-                                   args=(repeat, high_resolution,
-                                         self._logger, self._state))
+                                   args=(repeat,
+                                         high_resolution,
+                                         self._logger,
+                                         self._state))
         self._process.start()
 
         while self._state.value == 0:
-            time.sleep(0.1)
+            # time.sleep(0.1)
+            pass
 
     def wait(self, timeout=None):
         """
@@ -82,7 +95,7 @@ class StreamPlayer:
         Stop the streaming, by terminating the process.
         """
         if self._process:
-            self._logger.info(f"Stop streaming data from: {server_name}")
+            self._logger.info(f"Stop streaming data from: '{server_name}'.")
             self._process.terminate()
 
     def _stream(self, repeat, high_resolution, logger, state):
@@ -154,7 +167,7 @@ class StreamPlayer:
 
 class Streamer:
     """
-    Class for playing recorded file on LSL network.
+    Class for playing a recorded file on LSL network.
 
     Parameters
     ----------
@@ -239,8 +252,9 @@ class Streamer:
             self._sleep(high_resolution, idx_chunk, t_start, t_chunk)
 
             self._outlet.push_chunk(data)
-            self._logger.debug('[%8.3fs] sent %d samples (LSL %8.3f)' % (
-                time.perf_counter(), len(data), pylsl.local_clock()))
+            self._logger.debug(
+                '[%8.3fs] sent %d samples (LSL %8.3f)'
+                % (time.perf_counter(), len(data), pylsl.local_clock()))
 
             self._log_event(chunk)
             idx_chunk += 1
@@ -268,7 +282,6 @@ class Streamer:
         -------
         pylsl.StreamInfo
             The info to create the stream on LSL network.
-
         """
         sinfo = pylsl.StreamInfo(server_name, channel_count=self.get_nb_ch(),
                                  channel_format='float32',
@@ -278,32 +291,42 @@ class Streamer:
         desc = sinfo.desc()
         channel_desc = desc.append_child("channels")
         for ch in self.raw.ch_names:
-            channel_desc.append_child('channel').append_child_value('label', str(ch))\
-                .append_child_value('type', 'EEG').append_child_value('unit', 'microvolts')
+            channel_desc.append_child('channel')\
+                        .append_child_value('label', str(ch))\
+                        .append_child_value('type', 'EEG')\
+                        .append_child_value('unit', 'microvolts')
 
-        desc.append_child('amplifier').append_child(
-            'settings').append_child_value('is_slave', 'false')
-        desc.append_child('acquisition').append_child_value(
-            'manufacturer', 'NeuroDecode').append_child_value('serial_number', 'N/A')
+        desc.append_child('amplifier')\
+            .append_child('settings')\
+            .append_child_value('is_slave', 'false')
+
+        desc.append_child('acquisition')\
+            .append_child_value('manufacturer', 'NeuroDecode')\
+            .append_child_value('serial_number', 'N/A')
 
         return sinfo
 
     def load_data(self, fif_file):
         """
         Load the data to play from a fif file.
+        Multiplies all channel except trigger by 1e6 to convert to uV.
 
         Parameters
         ----------
         fif_file : str
             The absolute path to the .fif file to play.
         """
-        self._raw, self._events = load_fif_raw(fif_file)
-        self.raw._data[1:, :] = self._raw.get_data()[1:, :] * 1E6
+        self._raw, self._events = read_raw_fif(fif_file)
+
+        tch = self.get_trg_index()
+        idx = [k for k in range(self.raw._data.shape[0]) if k != tch]
+        self.raw._data[idx, :] = self._raw.get_data()[idx, :] * 1E6
 
         if self.raw is not None:
             self._logger.info_green(f'Successfully loaded {fif_file}')
         else:
-            raise RuntimeError(f'Error while loading {fif_file}')
+            self._logger.error(f"Error while loading '{fif_file}'.")
+            raise IOError
 
     def get_sample_rate(self):
         """
@@ -336,24 +359,28 @@ class Streamer:
         int
             The trigger channel's index.
         """
-        return find_event_channel(raw=self.raw)
+        return find_event_channel(inst=self.raw)
 
     def get_info(self):
         """
         Log the info about the created LSL stream.
         """
-        self._logger.info(f'Server name: {self.server_name}')
+        self._logger.info(
+            f'Server name: {self.server_name}')
         self._logger.info(
             f'Sampling frequency {self.get_sample_rate() = :.3f} Hz')
-        self._logger.info(f'Number of channels : {self.get_nb_ch()}')
-        self._logger.info(f'Chunk size : {self.chunk_size}')
+        self._logger.info(
+            f'Number of channels : {self.get_nb_ch()}')
+        self._logger.info(
+            f'Chunk size : {self.chunk_size}')
         for i, ch in enumerate(self.raw.ch_names):
             self._logger.info(f'{i} {ch}')
-        self._logger.info(f'Trigger channel : {self.get_trg_index()}')
+        self._logger.info(
+            f'Trigger channel : {self.get_trg_index()}')
 
     def _sleep(self, high_resolution, idx_chunk, t_start, t_chunk):
         """
-        Determine the time to sleep to fit simulate the
+        Determine the time to sleep.
         """
         if high_resolution:
             # if a resolution over 2 KHz is needed
@@ -385,7 +412,7 @@ class Streamer:
                                 f'Events: {event} ({self._tdef.by_value[event]})')
                         else:
                             self._logger.info(
-                                f'Events: {event} (Undefined event)')
+                                f'Events: {event} (Undefined event {event})')
 
     @property
     def raw(self):
@@ -423,7 +450,7 @@ class Streamer:
     @property
     def chunk_size(self):
         """
-         The size of a chunk of data.
+        The size of a chunk of data.
 
         Returns
         -------
@@ -433,6 +460,7 @@ class Streamer:
 
 
 if __name__ == '__main__':
+
     import sys
     from pathlib import Path
 
@@ -460,13 +488,10 @@ if __name__ == '__main__':
                 Path(input(">> Provide the path to the .fif file to play: \n>> ")))
 
     if len(sys.argv) == 1:
-        fif_file = str(
-            Path(input(">> Provide the path to the .fif file to play: \n>> ")))
         server_name = input(
             ">> Provide the server name displayed on LSL network: \n>> ")
-
-    if not fif_file.exists() or fif_file.suffix not in ['.fif', '.fiff']:
-        raise IOError("The file does not exist or is not a .fif file.")
+        fif_file = str(
+            Path(input(">> Provide the path to the .fif file to play: \n>> ")))
 
     sp = StreamPlayer(server_name, fif_file, chunk_size, trigger_file)
     sp.start()
