f720f58a90f301cd8c1366e7a86a47c9bb7c2106
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-06-09 16:50:13 +0200

    Changed to relative imports.

--- neurodecode/stream_viewer/stream_viewer.py
+++ neurodecode/stream_viewer/stream_viewer.py
@@ -2,13 +2,11 @@ import sys
 import time
 from PyQt5.QtWidgets import QApplication
 
-# TODO: Change to relative imports
-from neurodecode import logger
-from neurodecode.stream_receiver import StreamReceiver, StreamEEG
-from neurodecode.utils.lsl import search_lsl
-
-from _scope import _ScopeEEG
-from _scope_controller import _ScopeControllerUI
+from ._scope import _ScopeEEG
+from ._scope_controller import _ScopeControllerUI
+from .. import logger
+from ..stream_receiver import StreamReceiver, StreamEEG
+from ..utils.lsl import search_lsl
 
 
 class StreamViewer:
@@ -19,7 +17,8 @@ class StreamViewer:
         """
         Connect to the selected amplifier and plot the streamed data.
 
-        If stream infos are not provided, look for available streams on the network.
+        If stream infos are not provided, look for available streams on the
+        network.
         """
         if self.stream_name is None:
             self.stream_name = search_lsl(ignore_markers=True)
@@ -28,7 +27,7 @@ class StreamViewer:
         self.sr = StreamReceiver(bufsize=bufsize, winsize=bufsize,
                                  stream_name=self.stream_name)
         self.sr.streams[self.stream_name].blocking = False
-        time.sleep(bufsize) # Delay to fill the LSL buffer.
+        time.sleep(bufsize)  # Delay to fill the LSL buffer.
 
         if isinstance(self.sr.streams[self.stream_name], StreamEEG):
             self._scope = _ScopeEEG(self.sr, self.stream_name)
@@ -40,7 +39,3 @@ class StreamViewer:
         app = QApplication(sys.argv)
         self._ui = _ScopeControllerUI(self._scope)
         sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    viewer = StreamViewer('StreamPlayer')
-    viewer.start()
