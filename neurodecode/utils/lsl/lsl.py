c4701c7b8f064f5b12d096d77d502c6c8d9ebd29
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-06-17 15:54:16 +0200

    Improved utils.lsl. Removed state/logger variables and introduce a timeout variable.

--- neurodecode/utils/lsl/lsl.py
+++ neurodecode/utils/lsl/lsl.py
@@ -1,12 +1,11 @@
 """
 LSL wrapper functions for creating a server and a client.
 """
-import time
-import multiprocessing as mp
 import xml.etree.ElementTree as ET
 
 import pylsl
 
+from ..timer import Timer
 from ... import logger
 
 
@@ -26,19 +25,19 @@ def start_server(server_name, n_channels=1, channel_format='string',
         The channels' format.
             ('string', 'float32', 'double64', 'int8', 'int16',
              'int32', 'int64')
-    nominal_srate : float
+    nominal_srate : float | pylsl.IRREGULAR_RATE
         Sampling rate in Hz.
     stype : str
-        Signal type.
-        (https://github.com/sccn/xdf/wiki/Meta-Data#stream-content-types)
+        Signal type. Available type can be found here:
+        https://github.com/sccn/xdf/wiki/Meta-Data#stream-content-types
     source_id : str
         Unique identifier of the device or source of the data, if available
         (such as the serial number). If None, set to server name.
 
     Returns
     -------
-    LSL outlet :
-        LSL server object
+    pylsl.StreamOutlet
+        LSL outlet (server).
     """
     if source_id is None:
         source_id = server_name
@@ -48,42 +47,45 @@ def start_server(server_name, n_channels=1, channel_format='string',
                              channel_format=channel_format,
                              nominal_srate=nominal_srate,
                              type=stype, source_id=source_id)
+
     return pylsl.StreamOutlet(sinfo)
 
 
-def start_client(server_name, state=mp.Value('i', 1)):
+def start_client(stream_name, timeout=10):
     """
     Search for an LSL outlet (server) and open an LSL inlet (client).
 
     Parameters
     ----------
-    server_name: str
-        Name of the server to search.
-    state : Multiprocessing.Value
-        Used to stop searching from another process.
+    stream_name: str
+        Name of the stream to search.
+    timeout : int
+        Timeout duration in seconds after which the search of an LSL outlet
+        is interrupted.
 
     Returns
     -------
-    LSL inlet:
-        LSL client object.
+    pylsl.StreamInlet
+        LSL inlet (client).
     """
-    while state.value == 1:
-        logger.info(f'Searching for LSL server {server_name} ...')
-        streamInfos = pylsl.resolve_byprop("name", server_name, timeout=1)
+    logger.info(f'Searching for LSL stream {stream_name} ...')
+    streamInfos = pylsl.resolve_byprop("name", stream_name, timeout=timeout)
 
-        if not streamInfos:
-            continue
+    if len(streamInfos) == 0:
+        logger.error(f'No LSL stream found named {stream_name}.')
+        return None
 
-        for sinfo in streamInfos:
-            logger.info(f'Found {sinfo.name()}')
-        sinfo = streamInfos[0]
-        break
+    for sinfo in streamInfos:
+        logger.info(f'Found {sinfo.name()}')
+
+    if len(streamInfos) > 1:
+        logger.info('Selecting the first stream.')
+    sinfo = streamInfos[0]
 
     return pylsl.StreamInlet(sinfo)
 
 
-def list_lsl_streams(ignore_markers=False,
-                     logger=logger, state=mp.Value('i', 1)):
+def list_lsl_streams(ignore_markers=False):
     """
     List all the available outlets on LSL network.
 
@@ -91,68 +93,62 @@ def list_lsl_streams(ignore_markers=False,
     ----------
     ignore_markers : bool
         If True, ignore streams with Marker type.
-    logger : logging.Logger
-        The logger to output info.
-    state: mp.Value
-        The multiprocess sharing variable, used to stop search from another
-        process.
+
+    Returns
+    -------
+    stream_list : list
+        The list of the found stream name.
+    streamInfos : list
+        The list of pylsl.StreamInfo corresponding.
     """
-
-    # look for LSL servers
     stream_list = []
-    stream_list_markers = []
+    streamInfos = pylsl.resolve_streams()
 
-    while state.value == 1:
+    if len(streamInfos) == 0:
+        return stream_list, []
 
-        streamInfos = pylsl.resolve_streams()
+    for index, streamInfo in enumerate(streamInfos):
+        if ignore_markers and 'Markers' in streamInfo.type():
+            continue
+        stream_list.append(streamInfo.name())
 
-        if len(streamInfos) > 0:
-
-            for index, streamInfo in enumerate(streamInfos):
-                stream_name = streamInfo.name()
-                if 'Markers' in streamInfo.type():
-                    stream_list_markers.append((index, stream_name))
-                else:
-                    stream_list.append((index, stream_name))
-            break
-
-        logger.info('No server available yet on the network...')
-        time.sleep(1)
-
-    if ignore_markers is False:
-        stream_list += stream_list_markers
-
-    logger.info('-- List of servers --')
-
-    for i, (index, stream_name) in enumerate(stream_list):
-        logger.info(f'{i}: {stream_name}')
+    if ignore_markers:
+        streamInfos = [streamInfo for streamInfo in streamInfos
+                       if 'Markers' not in streamInfo.type()]
 
     return stream_list, streamInfos
 
 
-def search_lsl(ignore_markers=False, logger=logger, state=mp.Value('i', 1)):
+def search_lsl(ignore_markers=False, timeout=10):
     """
     Search and select an available stream on LSL network.
-
-    Does not open a LSL inlet.
+    Does not open an LSL inlet.
 
     Parameters
     ----------
     ignore_markers : bool
         If True, ignore streams with Marker type.
-    logger : logging.Logger
-        The logger to output info.
-    state: mp.Value
-        The multiprocess sharing variable, used to stop search from another
-        process.
+    timeout : int
+        Timeout duration in seconds after which the search of an LSL stream
+        is interrupted.
 
-    Returns:
-    --------
+    Returns
+    -------
     str : The selected stream name.
     """
-    stream_list, streamInfos = list_lsl_streams(ignore_markers, logger, state)
+    watchdog = Timer()
+    while watchdog.sec() <= timeout:
+        stream_list, streamInfos = list_lsl_streams(ignore_markers)
+        if len(stream_list) != 0:
+            break
 
-    if len(stream_list) == 1:
+    logger.info('-- List of servers --')
+    for i, stream_name in enumerate(stream_list):
+        logger.info(f'{i}: {stream_name}')
+
+    if len(stream_list) == 0:
+        logger.error('No LSL stream found on the network.')
+    elif len(stream_list) == 1:
         index = 0
     else:
         index = input(
@@ -163,8 +159,8 @@ def search_lsl(ignore_markers=False, logger=logger, state=mp.Value('i', 1)):
         else:
             index = int(index.strip())
 
-    stream_index, stream_name = stream_list[index]
-    streamInfo = streamInfos[stream_index]
+    stream_name = stream_list[index]
+    streamInfo = streamInfos[index]
     assert stream_name == streamInfo.name()
 
     logger.info(f'Selected: {stream_name}')
@@ -183,7 +179,8 @@ def lsl_channel_list(inlet):
 
     Returns:
     --------
-    list : List of channels name [ name1, name2, ... ]
+    list
+        List of channels name [name1, name2, ... ]
     """
     if not isinstance(inlet, pylsl.StreamInlet):
         logger.error(f'Wrong input type {type(inlet)}')
