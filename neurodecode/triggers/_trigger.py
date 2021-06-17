9c72fd8816aeb968cbc70f411a773ee3f74a62f0
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-06-17 18:01:18 +0200

    Reworked trigger module.

---
+++ neurodecode/triggers/_trigger.py
@@ -0,0 +1,42 @@
+"""
+Base class for triggers.
+"""
+from abc import ABC, abstractmethod
+
+from .. import logger
+
+
+class _Trigger(ABC):
+    """
+    Base trigger class.
+
+    Parameters
+    ----------
+    verbose : bool
+        If True, display a logger.info message when a trigger is sent.
+    """
+    @abstractmethod
+    def __init__(self, verbose=True):
+        self.verbose = verbose
+
+    @abstractmethod
+    def signal(self, value):
+        """
+        Send a trigger value.
+        """
+        if self.verbose:
+            logger.info(f'Sending trigger {value}.')
+
+    @abstractmethod
+    def _signal_off(self):
+        """
+        Reset trigger signal to 0.
+        """
+        self._set_data(0)
+
+    @abstractmethod
+    def _set_data(self, value):
+        """
+        Set the trigger signal to value.
+        """
+        pass
