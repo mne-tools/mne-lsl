273978f2ff351d8fcb9106b84ea1bd21e0035efc
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-07-08 20:38:18 +0200

    Reworked Control GUI for the StreamViewer. Events are broken?

--- neurodecode/stream_viewer/control_gui/control_eeg.py
+++ neurodecode/stream_viewer/control_gui/control_eeg.py
@@ -3,60 +3,71 @@ from pathlib import Path
 from configparser import RawConfigParser
 
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import (QMainWindow, QHeaderView,
-                             QTableWidgetItem, QFileDialog)
+from PyQt5.QtWidgets import QHeaderView, QTableWidgetItem, QFileDialog
 
-try:
-    from ..backends import _BackendVispy
-except ImportError:
-    pass
-from ..backends import _BackendPyQt5
-from ._ui_scope_controller import UI_MainWindow
+from ._control import _ControlGUI
+from ._ui_control import UI_MainWindow
+
 from ...stream_recorder import StreamRecorder
-from ... import logger
 
 
-class _ScopeControllerUI(QMainWindow):
+class ControlGUI_EEG(_ControlGUI):
     """
-    GUI controlling the scope and the backend.
+    Controller GUI for EEG LSL Stream.
 
     Parameters
     ----------
     scope : neurodecode.stream_viewer._scope._Scope
-        The scope connected to a stream receiver acquiring the data and
-        applying filtering. The scope has a buffer of _scope._BUFFER_DURATION
-        (default: 30s).
+        Scope connected to a stream receiver acquiring the data and applying
+        filtering. The scope has a buffer of _scope._BUFFER_DURATION.
     backend : str
         One of the supported backend's name. Supported 'vispy', 'pyqt5'.
-        If None, load the first backend in the order: Vispy, PyQt5.
     """
 
-    def __init__(self, scope, backend='pyqt5'):
-        super().__init__()
-        backend = _ScopeControllerUI._check_backend(backend)
-        self._backend = backend(scope)
+    def __init__(self, scope, backend):
+        super().__init__(scope, backend)
+        config_file = 'settings_scope_eeg.ini'
 
-        # Load UI
-        self._scope = scope
-        self._load_ui()
+        self._load_configuration(config_file)
+        self._load_gui()
+        self._init_backend(backend)
+        self._connect_signals_to_slots()
+        self._set_configuration(config_file)
 
-        # Load and set default configuration
-        self._set_init_configuration('settings_scope_eeg.ini')
+        self._backend.start_timer()
 
-        # Init backend
-        self._init_backend()
-
-    def _load_ui(self):
+    def _load_gui(self):
         """
         Loads the UI created with QtCreator.
         """
-        # Load
         self._ui = UI_MainWindow(self)
-        self._connect_signals_to_slots()
 
-        # Fill additional informations
-        self._fill_list_signal_y_scale(self._scope.signal_y_scales)
-        self._fill_table_channels()
+        for yRange in self._yRanges:
+            self._ui.comboBox_signal_yRange.addItem(str(yRange))
+
+        # Set table channels row/col
+        self._nb_table_columns = 8 if self._scope.nb_channels > 64 else 4
+        self._nb_table_rows = math.ceil(
+            self._scope.nb_channels / self._nb_table_columns)
+        self._ui.table_channels.setRowCount(self._nb_table_rows)
+        self._ui.table_channels.setColumnCount(self._nb_table_columns)
+
+        # Set table channels elements
+        for idx in range(self._scope.nb_channels):
+            row = idx // self._nb_table_columns
+            col = idx % self._nb_table_columns
+            self._ui.table_channels.setItem(
+                row, col, QTableWidgetItem(idx))
+            self._ui.table_channels.item(row, col).setTextAlignment(
+                QtCore.Qt.AlignCenter)
+            self._ui.table_channels.item(row, col).setText(
+                self._scope.channels_labels[idx])
+
+        # Table channels header
+        self._ui.table_channels.verticalHeader().setSectionResizeMode(
+            QHeaderView.Stretch)
+        self._ui.table_channels.horizontalHeader().setSectionResizeMode(
+            QHeaderView.Stretch)
 
         # Set position on the screen
         self.setGeometry(100, 100,
@@ -64,50 +75,36 @@ class _ScopeControllerUI(QMainWindow):
                          self.geometry().height())
         self.setFixedSize(self.geometry().width(),
                           self.geometry().height())
+        self.show() # Display
 
-        # Display
-        self.show()
-
-    def _fill_list_signal_y_scale(self, y_scales):
+    def _load_configuration(self, file):
         """
-        Fill the drop-down menu to select the signal range.
-
-        Parameters
-        ----------
-        y_scales : list of str
-            The list of items to place in the drop-down menu.
+        Load default configuration for the ranges.
         """
-        for y_scale in y_scales:
-            self._ui.comboBox_signal_y_scale.addItem(str(y_scale))
+        path2settings_folder = Path(__file__).parent / 'settings'
+        scope_settings = RawConfigParser(
+            allow_no_value=True, inline_comment_prefixes=('#', ';'))
+        scope_settings.read(str(path2settings_folder/file))
 
-    def _fill_table_channels(self):
-        """
-        Fill the table widget with the channel names.
-        """
-        self._nb_table_columns = 8 if self._scope.n_channels > 64 else 4
-        self._nb_table_rows = math.ceil(
-            self._scope.n_channels / self._nb_table_columns)
-        self._ui.table_channels.setRowCount(self._nb_table_rows)
-        self._ui.table_channels.setColumnCount(self._nb_table_columns)
+        # yRange (amplitude)
+        self._yRanges = {'1uV': 1, '10uV': 10, '25uV': 25,
+                         '50uV': 50, '100uV': 100, '250uV': 250,
+                         '500uV': 500, '1mV': 1000, '2.5mV': 2500,
+                         '10mV': 10000}
+        try:
+            self._yRange = float(scope_settings.get("plot", "yRange"))
+            if not self._yRange in self._yRanges.values:
+                raise ValueError
+        except Exception: # Default to 25 uV
+            self._yRange = 25.
 
-        for idx in range(self._scope.n_channels):
-            row = idx // self._nb_table_columns
-            col = idx % self._nb_table_columns
+        # xRange (time)
+        try:
+            self._xRange = int(scope_settings.get("plot", "xRange"))
+        except Exception: # Default to 10s
+            self._xRange = 10
 
-            self._ui.table_channels.setItem(
-                row, col, QTableWidgetItem(idx))
-            self._ui.table_channels.item(row, col).setTextAlignment(
-                QtCore.Qt.AlignCenter)
-            self._ui.table_channels.item(row, col).setText(
-                self._scope.channels_labels[idx])
-            self._ui.table_channels.item(row, col).setSelected(True)
-
-        self._ui.table_channels.verticalHeader().setSectionResizeMode(
-            QHeaderView.Stretch)
-        self._ui.table_channels.horizontalHeader().setSectionResizeMode(
-            QHeaderView.Stretch)
-
-    def _set_init_configuration(self, file):
+    def _set_configuration(self, file):
         """
         Load and set a default configuration for the GUI.
         """
@@ -116,34 +113,25 @@ class _ScopeControllerUI(QMainWindow):
             allow_no_value=True, inline_comment_prefixes=('#', ';'))
         scope_settings.read(str(path2settings_folder/file))
 
-        # Y-Scale (amplitude)
-        try:
-            scale = float(scope_settings.get("plot", "signal_y_scale"))
-            init_idx = list(self._scope.signal_y_scales.values()).index(scale)
-            self._ui.comboBox_signal_y_scale.setCurrentIndex(init_idx)
-        except Exception:
-            self._ui.comboBox_signal_y_scale.setCurrentIndex(0)
+        # yRange
+        init_idx = list(self._yRanges.values()).index(self._yRange)
+        self._ui.comboBox_signal_yRange.setCurrentIndex(init_idx)
 
-        # X-Scale (time)
+        # xRange
         try:
-            self._ui.spinBox_signal_x_scale.setValue(int(
-                scope_settings.get("plot", "time_plot")))
-        except Exception:
-            self._ui.spinBox_signal_x_scale.setValue(10)  # 10s by default
+            self._ui.spinBox_signal_xRange.setValue(self._xRange)
+        except: # 10s by default
+            self._ui.spinBox_signal_xRange.setValue(10)
 
-        # BP/CAR Filters
+        # CAR
         try:
             self._ui.checkBox_car.setChecked(bool(
                 scope_settings.get("filtering", "apply_car_filter")))
         except Exception:
             self._ui.checkBox_car.setChecked(False)
 
-        try:
-            self._ui.checkBox_bandpass.setChecked(bool(
-                scope_settings.get("filtering", "apply_bandpass_filter")))
-        except Exception:
-            self._ui.checkBox_bandpass.setChecked(False)
-
+        # BP Filters
+        self._ui.checkBox_bandpass.setChecked(True)
         try:
             self._ui.doubleSpinBox_bandpass_low.setValue(float(
                 scope_settings.get(
@@ -164,47 +152,42 @@ class _ScopeControllerUI(QMainWindow):
             low=self._ui.doubleSpinBox_bandpass_low.value(),
             high=self._ui.doubleSpinBox_bandpass_high.value())
 
-        # Events
+        # Trigger events
         try:
-            self._ui.checkBox_show_LPT_events.setChecked(
+            self._ui.checkBox_show_LPT_trigger_events.setChecked(
                 bool(scope_settings.get("plot", "show_LPT_events")))
         except Exception:
-            self._ui.checkBox_show_LPT_events.setChecked(False)
+            self._ui.checkBox_show_LPT_trigger_events.setChecked(False)
 
+        # Table channels
+        for idx in range(self._scope.nb_channels):
+            row = idx // self._nb_table_columns
+            col = idx % self._nb_table_columns
+            self._ui.table_channels.item(row, col).setSelected(True)
+
+        # Status bar
         self._ui.statusBar.showMessage("[Not recording]")
 
-    def _init_backend(self):
+    def _init_backend(self, backend):
         """
         Initialize the backend.
-
-        When calling .load_ui(), .connect_signals_to_slots() is called which
-        required both a scope and a backend to be defined.
-        However, the backend requires the _ScopeControllerUI to be fully
-        initialized and setup. Thus the backend is initialized in 2 steps:
-            - Creation of the instance (with all the associated methods)
-            - Initialization (with all the associated variables)
         """
-        x_scale = self._ui.spinBox_signal_x_scale.value()
-        y_scale_idx = self._ui.comboBox_signal_y_scale.currentIndex()
-        y_scale = list(self._scope.signal_y_scales.values())[y_scale_idx]
-        geometry = (self.geometry().x() + self.width(), self.geometry().y(),
-                    self.width() * 2, self.height())
-        self._backend.init_backend(
-            geometry, x_scale, y_scale, self.channels_to_show_idx)
-        self._backend.start_timer()
+        backend = _ControlGUI._check_backend(backend)
+        geometry = (self.geometry().x()+self.width(), self.geometry().y(),
+                    self.width()*2, self.height())
+        self._backend = backend(
+            self._scope, geometry, self._xRange, self._yRange)
 
-    # -------------------------------------------------------------------
-    # --------------------------- EVENT HANDLERS ------------------------
-    # -------------------------------------------------------------------
+    # --------------------------------------------------------------------
     def _connect_signals_to_slots(self):
         """
         Event handler. Connect QT signals to slots.
         """
         # Scales
-        self._ui.comboBox_signal_y_scale.activated.connect(
-            self.onActivated_comboBox_signal_y_scale)
-        self._ui.spinBox_signal_x_scale.valueChanged.connect(
-            self.onValueChanged_spinBox_signal_x_scale)
+        self._ui.comboBox_signal_yRange.activated.connect(
+            self.onActivated_comboBox_signal_yRange)
+        self._ui.spinBox_signal_xRange.valueChanged.connect(
+            self.onValueChanged_spinBox_signal_xRange)
 
         # CAR / Filters
         self._ui.checkBox_car.stateChanged.connect(
@@ -216,9 +199,9 @@ class _ScopeControllerUI(QMainWindow):
         self._ui.doubleSpinBox_bandpass_high.valueChanged.connect(
             self.onValueChanged_doubleSpinBox_bandpass_high)
 
-        # Events
-        self._ui.checkBox_show_LPT_events.stateChanged.connect(
-            self.onClicked_checkBox_show_LPT_events)
+        # Trigger events
+        self._ui.checkBox_show_LPT_trigger_events.stateChanged.connect(
+            self.onClicked_checkBox_show_LPT_trigger_events)
 
         # Recording
         self._ui.pushButton_start_recording.clicked.connect(
@@ -233,14 +216,15 @@ class _ScopeControllerUI(QMainWindow):
             self.onSelectionChanged_table_channels)
 
     @QtCore.pyqtSlot()
-    def onActivated_comboBox_signal_y_scale(self):
-        y_scale = list(self._scope.signal_y_scales.values())[
-            self._ui.comboBox_signal_y_scale.currentIndex()]
-        self._backend.y_scale = float(y_scale)
+    def onActivated_comboBox_signal_yRange(self):
+        self._yRange = float(list(self._yRanges.values())[
+            self._ui.comboBox_signal_yRange.currentIndex()])
+        self._backend.yRange = self._yRange
 
     @QtCore.pyqtSlot()
-    def onValueChanged_spinBox_signal_x_scale(self):
-        self._backend.x_scale = self._ui.spinBox_signal_x_scale.value()
+    def onValueChanged_spinBox_signal_xRange(self):
+        self._xRange = int(self._ui.spinBox_signal_xRange.value())
+        self._backend.xRange = self._xRange
 
     @QtCore.pyqtSlot()
     def onClicked_checkBox_car(self):
@@ -267,9 +251,9 @@ class _ScopeControllerUI(QMainWindow):
             high=self._ui.doubleSpinBox_bandpass_high.value())
 
     @QtCore.pyqtSlot()
-    def onClicked_checkBox_show_LPT_events(self):
+    def onClicked_checkBox_show_LPT_trigger_events(self):
         self._backend.show_LPT_events = bool(
-            self._ui.checkBox_show_LPT_events.isChecked())
+            self._ui.checkBox_show_LPT_trigger_events.isChecked())
 
     @QtCore.pyqtSlot()
     def onClicked_pushButton_start_recording(self):
@@ -301,12 +285,10 @@ class _ScopeControllerUI(QMainWindow):
     @QtCore.pyqtSlot()
     def onSelectionChanged_table_channels(self):
         selected = self._ui.table_channels.selectedItems()
-        self.channels_to_show_idx = [
+        self._scope.selected_channels = [
             item.row()*self._nb_table_columns + item.column()
             for item in selected]
-
-        self._scope.channels_to_show_idx = self.channels_to_show_idx
-        self._backend.channels_to_show_idx = self.channels_to_show_idx
+        self._backend.selected_channels = self._scope.selected_channels
 
     def closeEvent(self, event):
         """
@@ -315,61 +297,33 @@ class _ScopeControllerUI(QMainWindow):
         if self._ui.pushButton_stop_recording.isEnabled():
             self.onClicked_pushButton_stop_recording()
         self._backend.close()
-        event.accept()
+        super().closeEvent(event)
 
     # --------------------------------------------------------------------
-    @staticmethod
-    def _check_backend(backend):
-        """
-        Checks that the requested backend is supported.
-        """
-        SUPPORTED_BACKENDS = dict()
-        try:
-            SUPPORTED_BACKEND['vispy'] = _BackendVispy
-        except NameError:
-            pass
-        try:
-            SUPPORTED_BACKENDS['pyqt5'] = _BackendPyQt5
-        except NameError:
-            pass
-
-        if len(SUPPORTED_BACKENDS) == 0:
-            logger.error('The StreamViewer did not find an installed backend.')
-            raise RuntimeError
-
-        if isinstance(backend, str):
-            backend = backend.lower().strip()
-            if backend in SUPPORTED_BACKENDS:
-                return SUPPORTED_BACKENDS[backend]
-            else:
-                logger.warning(
-                    f"Selected backend '{backend}' is not supported. "
-                    "Default to 'pyqt5'.")
-                return SUPPORTED_BACKENDS['pyqt5']
-
-        else:
-            logger.warning(
-                "Selected backend is not a string. Default to 'pyqt5'.")
-            return SUPPORTED_BACKENDS['pyqt5']
-
-    # --------------------------------------------------------------------
-    @property
-    def scope(self):
-        """
-        The measuring scope.
-        """
-        return self._scope
-
     @property
     def backend(self):
         """
-        The display backend.
+        Display backend.
         """
         return self._backend
 
     @property
     def ui(self):
         """
-        The control UI.
+        Control UI.
         """
         return self._ui
+
+    @property
+    def xRange(self):
+        """
+        Selected X range (time) [seconds].
+        """
+        return self._xRange
+
+    @property
+    def yRange(self):
+        """
+        Selected Y range (amplitude) [uV].
+        """
+        return self._yRange
