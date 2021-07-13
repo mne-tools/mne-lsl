37dd7efa1ea98b6e22bd3d17881dcc37da54921b
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-07-13 18:49:05 +0200

    Rename io_file_dir file to _file_dir.

--- neurodecode/utils/io/__init__.py
+++ neurodecode/utils/io/__init__.py
@@ -11,5 +11,4 @@ For exporting, it supports:
 
 from .convert2fif import pcl2fif, any2fif, dir_any2fif
 from .export import write_set, dir_write_set
-from .io_file_dir import get_file_list, get_dir_list, make_dirs
 from .read_raw_fif import read_raw_fif, read_raw_fif_multi
