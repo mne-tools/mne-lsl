60f334f7e484265f13e6fd6c1abd94a0b7c0150d
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-08-08 11:33:12 +0200

    Simplified io.export functions.

--- neurodecode/utils/io/export.py
+++ neurodecode/utils/io/export.py
@@ -4,14 +4,8 @@ Export fif raw data to different format.
 Supported:
     - EEGLAB '.set'
 """
-from pathlib import Path
-
-import mne
 from scipy.io import savemat
 from numpy.core.records import fromarrays
-
-from ._file_dir import get_file_list, make_dirs
-from ... import logger
 
 
 # ----------------------------- EEG LAB -----------------------------
@@ -24,9 +18,9 @@ def write_set(raw, fname):
 
     Parameters
     ----------
-    raw : Raw | mne.io.RawArray
+    raw : Raw
         MNE instance of Raw.
-    fname : str
+    fname : str | Path
         Name/Path of the '.set' file created.
     """
     data = raw.get_data() * 1e6  # convert to microvolts
@@ -53,57 +47,3 @@ def write_set(raw, fname):
                           icasphere=[],
                           icaweights=[])),
             appendmat=False)
-
-
-def dir_write_set(fif_dir, recursive, out_dir=None):
-    """
-    Converts all raw fif file in a given directory to EEGLAB '.set' format.
-    The file name must respect MNE convention and end with '-raw.fif'.
-
-    Parameters
-    ----------
-    fif_dir : str
-         Path to the directory containing fif files.
-    recursive : bool
-        If true, search recursively.
-    out_dir : str | None
-        Path to the output directory. If None, the directory
-        'fif_dir/fif2set' is used.
-    """
-    fif_dir = Path(fif_dir)
-    if not fif_dir.exists():
-        logger.error(f"Directory '{fif_dir}' not found.")
-        raise IOError
-    if not fif_dir.is_dir():
-        logger.error(f"'{fif_dir}' is not a directory.")
-        raise IOError
-
-    if out_dir is None:
-        out_dir = 'fif2set'
-        if not (fif_dir / out_dir).is_dir():
-            make_dirs(fif_dir / out_dir)
-    else:
-        out_dir = Path(out_dir)
-        if not out_dir.is_dir():
-            make_dirs(out_dir)
-
-    for fif_file in get_file_list(fif_dir, fullpath=True, recursive=recursive):
-        fif_file = Path(fif_file)
-
-        if fif_file.suffix != '.fif':
-            continue
-        if not fif_file.stem.endswith('-raw'):
-            continue
-
-        raw = mne.io.read_raw(fif_file, preload=True)
-
-        relative = fif_file.relative_to(fif_dir).parent
-        if not (fif_dir / out_dir / relative).is_dir():
-            make_dirs(fif_dir / out_dir / relative)
-
-        logger.info(
-            "Exporting to "
-            "'{fif_dir / out_dir / relative / fif_file.stem}.set'")
-
-        write_set(raw, str(fif_dir / out_dir /
-                           relative / fif_file.stem) + '.set')
