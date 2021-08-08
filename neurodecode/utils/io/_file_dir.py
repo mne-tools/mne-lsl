3e57f0604fc1415b0cb869be858fc5c5d4c9155d
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-08-08 11:36:31 +0200

    Removed unused functions.

--- neurodecode/utils/io/_file_dir.py
+++ neurodecode/utils/io/_file_dir.py
@@ -5,110 +5,16 @@ from pathlib import Path
 from ... import logger
 
 
-def get_file_list(path, fullpath=True, recursive=False):
-    """
-    Get files with or without full path.
-
-    Parameters
-    ----------
-    path : str
-        Directory containing the files to list.
-    fullpath : bool
-        If True, returns the file's absolute path
-    recursive : bool
-        If True, search recursively
-
-    Returns
-    -------
-    list
-        File's list.
-    """
-    path = Path(path)
-    if not path.exists():
-        logger.error((f"The directory '{path}' does not exist."))
-        raise IOError
-
-    if not recursive:
-        if fullpath:
-            filelist = [
-                str(path / file.name) for file in os.scandir(path)
-                if (path / file.name).is_file() and file.name[0] != '.']
-        else:
-            filelist = [
-                file.name for file in os.scandir(path)
-                if (path / file.name).is_file() and file.name[0] != '.']
-
-    else:
-        filelist = []
-        for root, _, files in os.walk(path):
-            root = root.replace('\\', '/')
-            if fullpath:
-                for file in files:
-                    if file[0] != '.':
-                        filelist.append(str(Path(root) / file))
-            else:
-                for file in files:
-                    if file[0] != '.':
-                        filelist.append(file)
-
-    return sorted(filelist)
-
-
-def get_dir_list(path, recursive=False, no_child=False):
-    """
-    Get directory list relative to path.
-
-    Parameters
-    ----------
-    path : str
-        Directory contianing the directories to list.
-    recusrive : bool
-        If True, search recursively.
-    no_child : bool
-        If True, search directories having no child directory (leaf nodes)
-
-    Returns
-    -------
-    list
-        Directories' list
-    """
-    path = Path(path)
-    if not path.exists():
-        logger.error(f"The directory '{path}' does not exist.")
-        raise IOError
-
-    if not recursive:
-        dirlist = [str(path / file.name) for file in os.scandir(path)
-                   if (path / file.name).is_dir()]
-        if no_child:
-            dirlist2remove = [d for d in dirlist if len(get_dir_list(d)) > 0]
-            dirlist = [d for d in dirlist if d not in dirlist2remove]
-
-    else:
-        dirlist = []
-        for root, dirs, _ in os.walk(path):
-            root = root.replace('\\', '/')
-            for directory in dirs:
-                dirlist.append(str(Path(root) / directory))
-
-            if no_child:
-                dirlist2remove = [
-                    d for d in dirlist if len(get_dir_list(d)) > 0]
-                dirlist = [d for d in dirlist if d not in dirlist2remove]
-
-    return sorted(dirlist)
-
-
 def make_dirs(dirname, delete=False):
     """
     Create a new directory
 
     Parameters
     ----------
-    dirname : str
+    dirname : str | Path
         Name of the new directory.
     delete : bool
-        If True, if the directory already exists, it will be deleted
+        If True and the directory already exists, it will be deleted.
     """
     dirname = Path(dirname)
     if dirname.exists() and delete:
