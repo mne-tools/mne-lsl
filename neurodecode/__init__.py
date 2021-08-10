c8b59ded388246fda94688f5ae186f85535b76be
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-08-10 16:02:20 +0200

    Added imports for main Stream functions from root.

--- neurodecode/__init__.py
+++ neurodecode/__init__.py
@@ -3,3 +3,7 @@ NeuroDecode provides a real-time brain signal decoding framework.
 """
 from ._version import __version__
 from .logger import logger, set_log_level
+from .stream_receiver import StreamReceiver
+from .stream_recorder import StreamRecorder
+from .stream_player import StreamPlayer
+from .stream_viewer import StreamViewer
