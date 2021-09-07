a0cd5b6b47846c47ccfa6f02e1dec8ce4706486a
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-09-07 22:45:09 +0200

    Fix #15. Move path outside the function.
    Former-commit-id: c0d0a10256b96a5482fa12185cb14aec51685c44

--- bsl/datasets/sample.py
+++ bsl/datasets/sample.py
@@ -12,6 +12,7 @@ from ..utils.io._file_dir import make_dirs
 
 MD5 = '8925f81af22390fd17bb3341d553430f'
 URL = 'https://github.com/bsl-tools/bsl-datasets/raw/main/eeg/resting-state-sample-raw.fif'
+PATH = Path('~/bsl_data/eeg/resting-state-sample-raw.fif').expanduser()
 
 
 def data_path():
@@ -20,16 +21,15 @@ def data_path():
     If the dataset is not locally present, it is downloaded in the user home
     directory in the folder ``bsl-datasets``.
     """
-    path = Path('~/bsl_data/eeg/resting-state-sample-raw.fif').expanduser()
-    make_dirs(path.parent)
+    make_dirs(PATH.parent)
 
     logger.debug('URL:   %s' % (URL,))
     logger.debug('Hash:  %s' % (MD5,))
-    logger.debug('Path:  %s' % (path,))
+    logger.debug('Path:  %s' % (PATH,))
 
-    if path.exists() and _hashfunc(path) == MD5:
+    if PATH.exists() and _hashfunc(PATH) == MD5:
         download = False
-    elif path.exists() and not _hashfunc(path) == MD5:
+    elif PATH.exists() and not _hashfunc(PATH) == MD5:
         logger.warning(
             'Dataset existing but with different hash. Re-downloading.')
         download = True
@@ -38,6 +38,6 @@ def data_path():
         download = True
 
     if download:
-        fetch_file(URL, path, hash_=MD5, hash_type='md5', timeout=10.)
+        fetch_file(URL, PATH, hash_=MD5, hash_type='md5', timeout=10.)
 
-    return path
+    return PATH
