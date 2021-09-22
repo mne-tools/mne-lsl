18aa0d93d277787c72dde8fd080a58627af03e27
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-09-22 14:10:28 +0200

    Renamed downloaded readme in tests.

--- bsl/datasets/tests/test_fetching.py
+++ bsl/datasets/tests/test_fetching.py
@@ -15,11 +15,11 @@ WRONG_HASH = {'md5': '12345678912345678912345678912345',
 @pytest.mark.parametrize('hash_type', ('md5', 'sha1'))
 def test_fetch_file(tmp_path, hash_type):
     """Test file download."""
-    fetch_file(URL, tmp_path / 'README.md',
+    fetch_file(URL, tmp_path / 'README1.md',
                hash_=HASH[hash_type], hash_type=hash_type)
     # Test wrong hash value
     with pytest.raises(RuntimeError, match='Hash mismatch for downloaded'):
-        fetch_file(URL, tmp_path / 'README3.md',
+        fetch_file(URL, tmp_path / 'README2.md',
                    hash_=WRONG_HASH[hash_type], hash_type=hash_type)
 
 
@@ -34,7 +34,7 @@ def test_fetch_file_invalid_arg(tmp_path):
         fetch_file(URL, tmp_path / 'test.md', hash_=123456789)
     # Test invalid hash type
     with pytest.raises(ValueError, match='Unsupported hash type'):
-        fetch_file(URL, tmp_path / 'README1.md',
+        fetch_file(URL, tmp_path / 'README3.md',
                    hash_=HASH['md5'], hash_type='...')
-        fetch_file(URL, tmp_path / 'README1.md',
+        fetch_file(URL, tmp_path / 'README3.md',
                    hash_=HASH['md5'], hash_type=123)
