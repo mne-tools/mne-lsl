d7cd3f09fe49356d8504661676a307b0f8a59603
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-09-23 13:52:20 +0200

    Added test for stream player.

---
+++ bsl/stream_player/tests/test_stream_player.py
@@ -0,0 +1,50 @@
+import time
+import pylsl
+
+from bsl import StreamPlayer, logger, set_log_level
+from bsl.datasets import sample
+from bsl.utils._testing import requires_sample_dataset
+
+
+set_log_level('INFO')
+logger.propagate = True
+
+
+@requires_sample_dataset
+def test_stream_player(caplog):
+    """Test stream player capabilities."""
+    sp = StreamPlayer('StreamPlayer', sample.data_path())
+    sp.start(repeat=float('inf'), high_resolution=False)
+    time.sleep(2)
+    streams = pylsl.resolve_streams()
+    assert sp.stream_name in [stream.name() for stream in streams]
+    sp.stop()
+    assert sp.process is None
+    assert 'Stop streaming data' in caplog.text
+
+    # Test properties
+    sp.stream_name = 'StreamPlayer2'
+    assert sp.stream_name == 'StreamPlayer2'
+
+    sp.fif_file = sample.data_path()
+    assert sp.fif_file == sample.data_path()
+
+    sp.chunk_size = 32
+    assert sp.chunk_size == 32
+
+    # TODO: Add trigger_file test once its added to datasets
+
+    # Test property setters
+    sp.start(high_resolution=False)
+    sp.stream_name = 'StreamPlayer'
+    assert 'Stop the stream before changing the name' in caplog.text
+    sp.fif_file = sample.data_path()
+    assert 'Stop the stream before changing the FIF file' in caplog.text
+    sp.chunk_size = 16
+    assert 'Stop the stream before changing the chunk size' in caplog.text
+    sp.stop()
+
+    # Test high_resolution
+    sp.start(high_resolution=True)
+    time.sleep(2)
+    sp.stop()
