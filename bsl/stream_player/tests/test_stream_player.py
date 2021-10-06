d17b6b567b2c518566dc90bbf0807f841c8420b9
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2021-10-06 14:58:46 +0200

    Improve the StreamPlayer. (#30)
    * Remove property setters to simplify API.
    
    * Added state value.
    
    * Changed property definition order for consistency.
    
    * Reorder arguments and moved repeat and high_resolution to initialization.
    
    * Rename trigger_file argument to trigger_def and add static checker.
    
    * Added checker for repeat.
    
    * Added logger warnings to checkers.
    
    * Improve checker for chunk_size.
    
    * Fix typo.
    
    * Changed logging error for attribute ignored/modified.
    
    * Added checker for fif file to check at assignment.
    
    * Changed logger message to lazy evaluation.
    
    * Rework of _Streamer creation.
    
    * Rework _Streamer streaming methods.
    
    * typo corrected.
    
    * Rework stop stream player.
    
    * Remove obsolete fill_doc decorator.
    
    * Changed invalid param to valid.
    
    * Remove old tests and added tests for properties and checkers.
    
    * fix typo.
    
    * Added test for default behavior and streaming.
    
    * Adapted CLI parser and command.
    
    * Convert fif_file to Path.
    
    * Fix missing return.
    
    * Renamed placeholder for test with a fif file with triggers.
    
    * Improve checkers and test coverage.
    
    * Updated documentation.
    
    * Fix documentation typos.
    
    * doc typo corrected in utils.lsl
    
    Former-commit-id: 4b95dc462608935cb115466d828ac96c698c847a

--- bsl/stream_player/tests/test_stream_player.py
+++ bsl/stream_player/tests/test_stream_player.py
@@ -1,8 +1,14 @@
 import time
+from pathlib import Path
+import multiprocessing as mp
+
+import mne
 import pylsl
+import pytest
 
 from bsl import StreamPlayer, logger, set_log_level
 from bsl.datasets import sample, event
+from bsl.triggers import TriggerDef
 from bsl.utils._testing import requires_sample_dataset, requires_event_dataset
 
 
@@ -10,45 +16,262 @@ set_log_level('INFO')
 logger.propagate = True
 
 
-@requires_event_dataset
 @requires_sample_dataset
-def test_stream_player(caplog):
-    """Test stream player capabilities."""
-    sp = StreamPlayer('StreamPlayer', sample.data_path())
-    sp.start(repeat=float('inf'), high_resolution=False)
-    time.sleep(2)
+def test_stream_player_default(caplog):
+    """Test stream player default capabilities."""
+    stream_name = 'StreamPlayer'
+    fif_file = sample.data_path()
+    raw = mne.io.read_raw_fif(fif_file, preload=True)
+
+    # Test start
+    caplog.clear()
+    sp = StreamPlayer(stream_name=stream_name, fif_file=fif_file)
+    sp.start()
+    assert 'Streaming started.' in caplog.text
+
+    # Test stream is in resolved streams
+    streams = pylsl.resolve_streams()
+    assert stream_name in [stream.name() for stream in streams]
+
+    # Test that data is being streamed
+    idx = [stream.name() for stream in streams].index(stream_name)
+    inlet = pylsl.StreamInlet(streams[idx], max_buflen=int(raw.info['sfreq']))
+    inlet.open_stream()
+    time.sleep(0.05)
+    chunk, tslist = inlet.pull_chunk(
+        timeout=0.0, max_samples=int(raw.info['sfreq']))
+    assert len(chunk) == len(tslist)
+    assert 0 < len(chunk) < int(raw.info['sfreq'])
+    time.sleep(1)
+    chunk, tslist = inlet.pull_chunk(
+        timeout=0.0, max_samples=int(raw.info['sfreq']))
+    assert len(chunk) == len(tslist) ==  int(raw.info['sfreq'])
+
+    # Test stop
+    sp.stop()
+    assert ('Waiting for StreamPlayer %s process to finish.'
+            % 'StreamPlayer') in caplog.text
+    assert sp.process is None
+
+    # Test restart/stop
+    caplog.clear()
+    sp.start()
+    assert 'Streaming started.' in caplog.text
     streams = pylsl.resolve_streams()
     assert sp.stream_name in [stream.name() for stream in streams]
     sp.stop()
+    assert ('Waiting for StreamPlayer %s process to finish.'
+            % 'StreamPlayer') in caplog.text
     assert sp.process is None
-    assert 'Stop streaming data' in caplog.text
 
-    # Test properties
-    sp.stream_name = 'StreamPlayer2'
-    assert sp.stream_name == 'StreamPlayer2'
+    # Test stop when not started
+    caplog.clear()
+    sp.stop()
+    assert 'StreamPlayer was not started. Skipping.' in caplog.text
 
-    sp.fif_file = sample.data_path()
+
+@requires_event_dataset
+@requires_sample_dataset
+def test_stream_player_triggers():
+    """Test stream player trigger display capabilities."""
+    pass
+
+
+@requires_sample_dataset
+def test_stream_player_high_resolution():
+    """Test stream player high-resolution capabilities."""
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      high_resolution=True)
+    assert sp.high_resolution is True
+    sp.start()
+    assert sp.high_resolution is True
+    time.sleep(0.1)
+    sp.stop()
+    assert sp.high_resolution is True
+
+
+@requires_sample_dataset
+def test_stream_player_properties():
+    """Test the stream_player properties."""
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      repeat=float('inf'), trigger_def=None, chunk_size=16,
+                      high_resolution=False)
+
+    # Check getters
+    assert sp.stream_name == 'StreamPlayer'
     assert sp.fif_file == sample.data_path()
+    assert sp.repeat == float('inf')
+    assert sp.trigger_def is None
+    assert sp.chunk_size == 16
+    assert sp.high_resolution is False
 
-    sp.chunk_size = 32
+    # Check setters
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        sp.stream_name = 'new name'
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        sp.fif_file = 'new fif file'
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        sp.repeat = 10
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        sp.trigger_def = TriggerDef()
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        sp.chunk_size = 8
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        sp.high_resolution = True
+
+    # Process and state
+    assert sp.process is None
+    assert isinstance(sp.state, mp.sharedctypes.Synchronized)
+    assert sp.state.value == 0
+
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        sp.process = 5
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        sp.state = 5
+
+    sp.start()
+    assert isinstance(sp.process, mp.context.Process)
+    assert sp.state.value == 1
+
+    sp.stop()
+    assert sp.process is None
+    assert sp.state.value == 0
+
+
+def test_stream_player_invalid_fif_file():
+    """Test that initialization fails if argument fif_file is invalid."""
+    with pytest.raises(ValueError, match='Argument fif_file must be'):
+        StreamPlayer(stream_name='StreamPlayer', fif_file='non-existing-path')
+    with pytest.raises(ValueError, match='Argument fif_file must be'):
+        StreamPlayer(stream_name='StreamPlayer', fif_file=5)
+
+
+@requires_sample_dataset
+def test_stream_player_checker_repeat(caplog):
+    """Test the checker for argument repeat."""
+    # Default
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      repeat=float('inf'))
+    assert sp.repeat == float('inf')
+
+    # Positive integer
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      repeat=5)
+    assert sp.repeat == 5
+
+    # Positive float
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      repeat=5.)
+    assert isinstance(sp.repeat, int) and sp.repeat == 5
+
+    # Negative number
+    caplog.clear()
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      repeat=-5)
+    assert ('Argument repeat must be a strictly positive integer. '
+            'Provided: %s -> Changing to +inf.' % -5) in caplog.text
+    assert sp.repeat == float('inf')
+
+    # Not convertible to integer
+    caplog.clear()
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      repeat=[1, 2])
+    assert ('Argument repeat must be a strictly positive integer. '
+            'Provided: %s -> Changing to +inf.' % str([1, 2])) in caplog.text
+    assert sp.repeat == float('inf')
+
+
+@requires_event_dataset
+@requires_sample_dataset
+def test_stream_player_checker_trigger_def(caplog):
+    """Test the checker for argument trigger_def."""
+    # Default
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      trigger_def=None)
+    assert sp.trigger_def is None
+
+    # TriggerDef instance
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      trigger_def=TriggerDef(trigger_file=None))
+    assert isinstance(sp.trigger_def, TriggerDef)
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      trigger_def=TriggerDef(trigger_file=event.data_path()))
+    assert isinstance(sp.trigger_def, TriggerDef)
+
+    # Path as a string
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      trigger_def=str(event.data_path()))
+    assert isinstance(sp.trigger_def, TriggerDef)
+
+    # Path as a Path object
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      trigger_def=Path(event.data_path()))
+    assert isinstance(sp.trigger_def, TriggerDef)
+
+    # Path to a non-existing file
+    caplog.clear()
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      trigger_def='non-existing-path')
+    assert ('Argument trigger_def is a path that does not exist. '
+            'Provided: %s -> Ignoring.' % 'non-existing-path') in caplog.text
+    assert sp.trigger_def is None
+
+    # Invalid type
+    caplog.clear()
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      trigger_def=5)
+    assert ('Argument trigger_def must be a TriggerDef instance or a path '
+            'to a trigger definition ini file. '
+            'Provided: %s -> Ignoring.' % type(5)) in caplog.text
+    assert sp.trigger_def is None
+
+
+@requires_sample_dataset
+def test_stream_player_checker_chunk_size(caplog):
+    """Test the checker for argument chunk_size."""
+    # Default
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      chunk_size=16)
+    assert sp.chunk_size == 16
+
+    # Positive integer
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      chunk_size=32)
     assert sp.chunk_size == 32
 
-    sp.trigger_file = event.data_path()
-    assert sp.trigger_file == event.data_path()
+    # Positive float
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      chunk_size=32.)
+    assert isinstance(sp.chunk_size, int) and sp.chunk_size == 32
 
-    # Test property setters
-    sp.start(high_resolution=False)
-    sp.stream_name = 'StreamPlayer'
-    assert 'Stop the stream before changing the name' in caplog.text
-    sp.fif_file = sample.data_path()
-    assert 'Stop the stream before changing the FIF file' in caplog.text
-    sp.chunk_size = 16
-    assert 'Stop the stream before changing the chunk size' in caplog.text
-    sp.trigger_file = None
-    assert 'Stop the stream before changing the trigger file' in caplog.text
-    sp.stop()
+    # Positive non-usual integer
+    caplog.clear()
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      chunk_size=8)
+    assert ('The chunk size %s is different from the usual '
+            'values 16 or 32.' % 8) in caplog.text
+    assert sp.chunk_size == 8
 
-    # Test high_resolution
-    sp.start(high_resolution=True)
-    time.sleep(2)
-    sp.stop()
+    # Negative number
+    caplog.clear()
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      chunk_size=-8)
+    assert ('Argument chunk_size must be a strictly positive integer. '
+            'Provided: %s -> Changing to 16.' % -8) in caplog.text
+    assert sp.chunk_size == 16
+
+    # Not convertible to integer
+    caplog.clear()
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      chunk_size=[1, 2])
+    assert ('Argument chunk_size must be a strictly positive integer. '
+            'Provided: %s -> Changing to 16.' % str([1, 2])) in caplog.text
+    assert sp.chunk_size == 16
+
+    # Infinite
+    caplog.clear()
+    sp = StreamPlayer(stream_name='StreamPlayer', fif_file=sample.data_path(),
+                      chunk_size=float('inf'))
+    assert ('Argument chunk_size must be a strictly positive integer. '
+            'Provided: %s -> Changing to 16.' % float('inf')) in caplog.text
+    assert sp.chunk_size == 16
