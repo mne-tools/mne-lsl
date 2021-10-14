e86127e492adbcc5ae8dd2fc0c347e5feef273aa
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-10-14 10:53:36 +0200

    Remove verbose argument.

--- bsl/stream_receiver/stream_receiver.py
+++ bsl/stream_receiver/stream_receiver.py
@@ -183,7 +183,7 @@ class StreamReceiver:
             self._acquisition_threads[stream] = thread
 
     @fill_doc
-    def get_window(self, stream_name=None, return_raw=False, verbose=True):
+    def get_window(self, stream_name=None, return_raw=False):
         """
         Get the latest window from a stream's buffer.
         If several streams are connected, specify the name.
@@ -192,7 +192,6 @@ class StreamReceiver:
         ----------
         %(receiver_get_stream_name)s
         %(receiver_get_return_raw)s
-        %(receiver_get_verbose)s
 
         Returns
         -------
@@ -242,17 +241,16 @@ class StreamReceiver:
                     'The LSL stream %s can not be converted to MNE raw '
                     'instance. Returning numpy arrays.', stream_name)
         else:
-            if verbose:  # Needed for StreamViewer
-                logger.warning(
-                    'The LSL stream %s did not return any data.'
-                    'Returning empty numpy arrays.', stream_name)
+            logger.warning(
+                'The LSL stream %s did not return any data.'
+                'Returning empty numpy arrays.', stream_name)
             window = np.empty((0, len(self._streams[stream_name].ch_list)))
             timestamps = np.array([])
 
         return window, timestamps
 
     @fill_doc
-    def get_buffer(self, stream_name=None, return_raw=False, verbose=True):
+    def get_buffer(self, stream_name=None, return_raw=False):
         """
         Get the entire buffer of a stream.
         If several streams are connected, specify the name.
@@ -261,7 +259,6 @@ class StreamReceiver:
         ----------
         %(receiver_get_stream_name)s
         %(receiver_get_return_raw)s
-        %(receiver_get_verbose)s
 
         Returns
         -------
@@ -304,10 +301,9 @@ class StreamReceiver:
                     'The LSL stream %s can not be converted to MNE raw '
                     'instance. Returning numpy arrays.', stream_name)
         else:
-            if verbose:  # Needed for StreamViewer
-                logger.warning(
-                    'The LSL stream %s did not return any data.'
-                    'Returning empty numpy arrays.', stream_name)
+            logger.warning(
+                'The LSL stream %s did not return any data.'
+                'Returning empty numpy arrays.', stream_name)
             window = np.empty((0, len(self._streams[stream_name].ch_list)))
             timestamps = np.array([])
 
