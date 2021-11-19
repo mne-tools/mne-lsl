6d35789f403ec7555a657633acc873f10721f6af
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2021-11-19 16:36:09 +0100

    Fix #43 - Add validation test for EEG scope. (#44)
    * Added tests for EEG scope.
    
    * fix typo in warning.
    
    * Improved find_event_channels capabilities.
    
    * Improve structure of find_event_channels and remove Evoked as trigger channel can not be part of an Evoked instance.
    
    * fix typo.
    
    * improve docstring.
    
    * Improve logger messages in io.

--- bsl/utils/find_event_channel.py
+++ bsl/utils/find_event_channel.py
@@ -1,12 +1,11 @@
 import numpy as np
 from mne.io import BaseRaw
-from mne.evoked import Evoked
 from mne.epochs import BaseEpochs
 
 from ._checks import _check_type
 
 
-def find_event_channel(inst=None, ch_names=None):  # noqa: E501
+def find_event_channel(inst=None, ch_names=None):
     """
     Find the event channel using heuristics.
 
@@ -14,56 +13,74 @@ def find_event_channel(inst=None, ch_names=None):  # noqa: E501
 
         Not 100% guaranteed to find it.
         If ``inst`` is `None`, ``ch_names`` must be given.
+        If ``inst`` is an MNE instance, ``ch_names`` is ignored if some
+        channels types are ``'stim'``.
 
     Parameters
     ----------
-    inst : `None` | `~mne.io.Raw` | `~mne.Epochs` | `~mne.Evoked` | `~numpy.array` ``(n_channels, n_samples)``
-        Data instance.
+    inst : `None` | `~mne.io.Raw` | `~mne.Epochs` | `~numpy.array`
+        Data instance. If a `~numpy.array` is provided, the shape must be
+        ``(n_channels, n_samples)``.
     ch_names : `None` | `list`
         Channels name list.
 
     Returns
     -------
-    `int` | `None`
-        Event channel index or `None` if not found.
+    `int` | `list` | `None`
+        Event channel index, list of event channel indexes or `None` if not
+        found.
     """
-    _check_type(inst, (None, np.ndarray, BaseRaw, BaseEpochs, Evoked),
-                item_name='inst')
+    _check_type(inst, (None, np.ndarray, BaseRaw, BaseEpochs), item_name='inst')
     _check_type(ch_names, (None, list, tuple), item_name='ch_names')
 
-    valid_trigger_ch_names = ['TRIGGER', 'STI', 'TRG', 'CH_Event']
+    # numpy array + ch_names
+    if isinstance(inst, np.ndarray) and ch_names is not None:
+        tchs = _search_in_ch_names(ch_names)
 
-    # For numpy array
-    if isinstance(inst, np.ndarray):
-        if ch_names is not None:
-            for ch_name in ch_names:
-                if any(trigger_ch_name in ch_name
-                       for trigger_ch_name in valid_trigger_ch_names):
-                    return ch_names.index(ch_name)
-
+    # numpy array without ch_names
+    elif isinstance(inst, np.ndarray) and ch_names is None:
         # data range between 0 and 255 and all integers?
-        for ch_idx in range(inst.shape[0]):
-            if (inst[ch_idx].astype(int, copy=False) == inst[ch_idx]).all() \
-                    and max(inst[ch_idx]) < 256 and min(inst[ch_idx]) == 0:
-                return ch_idx
+        tchs = [idx for idx in range(inst.shape[0])
+                if (inst[idx].astype(int, copy=False) == inst[idx]).all()
+                and max(inst[idx]) <= 255 and min(inst[idx]) == 0]
 
-    # For MNE inst
-    elif hasattr(inst, 'ch_names'):
-        if 'stim' in inst.get_channel_types():
-            return inst.get_channel_types().index('stim')
+    # For MNE raw/epochs + ch_names
+    elif isinstance(inst, (BaseRaw, BaseEpochs)) and ch_names is not None:
+        tchs = [idx for idx, type_ in enumerate(inst.get_channel_types())
+                if type_ == 'stim']
+        if len(tchs) == 0:
+            tchs = _search_in_ch_names(ch_names)
 
-        for ch_name in inst.ch_names:
-            if any(trigger_ch_name in ch_name
-                   for trigger_ch_name in valid_trigger_ch_names):
-                return inst.ch_names.index(ch_name)
+    # For MNE raw/epochs without ch_names
+    elif isinstance(inst, (BaseRaw, BaseEpochs)) and ch_names is None:
+        tchs = [idx for idx, type_ in enumerate(inst.get_channel_types())
+                if type_ == 'stim']
+        if len(tchs) == 0:
+            tchs = _search_in_ch_names(inst.ch_names)
 
     # For unknown data type
-    else:
+    elif inst is None:
         if ch_names is None:
             raise ValueError('ch_names cannot be None when inst is None.')
-        for ch_name in ch_names:
-            if any(trigger_ch_name in ch_name
-                   for trigger_ch_name in valid_trigger_ch_names):
-                return ch_names.index(ch_name)
+        tchs = _search_in_ch_names(ch_names)
 
-    return None
+    # output
+    if len(tchs) == 0:
+        return None
+    elif len(tchs) == 1:
+        return tchs[0]
+    else:
+        return tchs
+
+
+def _search_in_ch_names(ch_names):
+    """Search trigger channel by name in a list of valid names."""
+    valid_trigger_ch_names = ['TRIGGER', 'STI', 'TRG', 'CH_Event']
+
+    tchs = list()
+    for idx, ch_name in enumerate(ch_names):
+        if any(trigger_ch_name in ch_name
+               for trigger_ch_name in valid_trigger_ch_names):
+            tchs.append(idx)
+
+    return tchs
