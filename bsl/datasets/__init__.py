14b31a597e4ebf3fa6e3c8665a06cb0a6d4713d7
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-11-20 13:15:04 +0100

    Add pytest actions and add utils to download all datasets.

--- bsl/datasets/__init__.py
+++ bsl/datasets/__init__.py
@@ -4,3 +4,16 @@ from . import eeg_auditory_stimuli  # noqa: F401
 from . import eeg_resting_state  # noqa: F401
 from . import eeg_resting_state_short  # noqa: F401
 from . import trigger_def  # noqa: F401
+
+
+def _download_all_datasets(verbose=False):
+    """
+    Download all the datasets.
+    """
+    eeg_auditory_stimuli.data_path()
+    eeg_resting_state.data_path()
+    eeg_resting_state_short.data_path()
+    trigger_def.data_path()
+
+    if verbose:
+        print ('Download completed.')
