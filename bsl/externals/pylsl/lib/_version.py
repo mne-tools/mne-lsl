a25b71532d0087f6e24cfeab517f6b4a8f6f6e99
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-02-17 18:25:09 +0100

    remove windows 32 bits version

--- bsl/externals/pylsl/lib/_version.py
+++ bsl/externals/pylsl/lib/_version.py
@@ -3,7 +3,7 @@
 .deb is the linux (focal) version.
 64.deb is the linux (bionic) version.
 .dylib is the macOS version.
-.dll is the windows 32 and 64 bits version.
+.dll is the windows 64 bits version.
 """
 
 __version__ = '1.15.2'
