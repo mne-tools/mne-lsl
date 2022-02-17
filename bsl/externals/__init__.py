d56d45ee7d3c21373121fe5360a3db76caa2216f
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-02-17 15:03:14 +0100

    improves loading of pylsl

--- bsl/externals/__init__.py
+++ bsl/externals/__init__.py
@@ -2,9 +2,31 @@
 External packages bundle together with BSL.
 """
 import importlib
+from ctypes import CDLL
 
 
 try:
+    # is pylsl installed?
     pylsl = importlib.import_module(name='pylsl')
-except (ModuleNotFoundError, ImportError):
+    # does it find a liblsl?
+    libpaths = pylsl.pylsl.find_liblsl_libraries()
+    while True:
+        try:
+            libpath = next(libpaths)
+            lib = CDLL(libpath)
+        except StopIteration:
+            raise StopIteration
+        except Exception:
+            continue
+        finally:
+            # clean up
+            try:
+                del libpath
+            except NameError:
+                pass
+            try:
+                del lib
+            except NameError:
+                pass
+except (ModuleNotFoundError, ImportError, StopIteration):
     pylsl = importlib.import_module(name='.pylsl', package=__name__)
