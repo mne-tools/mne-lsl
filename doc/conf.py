9aa59bf00632984f34631ac84ddb5bbd4a2447b2
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-02-18 18:57:40 +0100

    fix path

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ from datetime import datetime, timezone
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-sys.path.append(str(Path(__file__).parent.parent / 'bsl'))
+sys.path.append(str(Path(__file__).parent.parent))
 sys.path.append(str(Path(__file__).parent / 'sphinxext'))
 import bsl
 
