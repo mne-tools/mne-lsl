d9e233fe8985d8c69c649face288f2e2a54e12aa
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-02-18 11:55:43 +0100

    add __all__ to modules
    Former-commit-id: 163db1c927b7f3a6ae1e9349cc4f56a23eee211f

--- bsl/triggers/__init__.py
+++ bsl/triggers/__init__.py
@@ -2,3 +2,9 @@ from .mock import MockTrigger  # noqa: F401
 from .parallel import ParallelPortTrigger  # noqa: F401
 from .software import SoftwareTrigger  # noqa: F401
 from .trigger_def import TriggerDef  # noqa: F401
+
+
+__all__ = ['MockTrigger',
+           'ParallelPortTrigger',
+           'SoftwareTrigger',
+           'TriggerDef']
