c6a6fe372566397552632636829968ecc385a145
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-02-18 11:55:43 +0100

    add __all__ to modules

--- bsl/stream_receiver/__init__.py
+++ bsl/stream_receiver/__init__.py
@@ -5,5 +5,8 @@ Acquire signals from LSL streams. Used by other modules such as StreamViewer
 and StreamRecorder.
 """
 
-from ._stream import StreamMarker, StreamEEG  # noqa: F401
+from ._stream import StreamEEG, StreamMarker  # noqa: F401
 from .stream_receiver import StreamReceiver  # noqa: F401
+
+
+__all__ = ['StreamReceiver', 'StreamEEG', 'StreamMarker']
