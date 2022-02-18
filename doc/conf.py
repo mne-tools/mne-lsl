91c896dffc6686ccd525c3c7f7abcbd323fb04cb
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-02-18 17:45:27 +0100

    fix typo

--- doc/conf.py
+++ doc/conf.py
@@ -10,14 +10,12 @@ import sys
 from pathlib import Path
 from datetime import datetime, timezone
 
-import bsl
-
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 sys.path.append(str(Path(__file__).parent.parent / 'bsl'))
 sys.path.append(str(Path(__file__).parent / 'sphinxext'))
 import bsl
