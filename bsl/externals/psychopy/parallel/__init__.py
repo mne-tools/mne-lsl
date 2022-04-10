d7b99849ac07df2b1f2c10ab63133f37fea999fd
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-04-10 11:42:58 +0200

    fix import of ParallelPort from psychopy and change logger warning into RuntimeError when parallel port is missing
    Former-commit-id: 8fefcf82985f0d19fe92a9dcabafdc1d4da61913

--- bsl/externals/psychopy/parallel/__init__.py
+++ bsl/externals/psychopy/parallel/__init__.py
@@ -22,8 +22,6 @@ parallel port.
 
 import sys
 
-from .... import logger
-
 # To make life easier, only try drivers which have a hope in heck of working.
 # Because hasattr() in connection to windll ends up in an OSError trying to
 # load 32bit drivers in a 64bit environment, different drivers defined in
@@ -49,68 +47,9 @@ elif sys.platform == 'win32':
             ParallelPort = None
             continue
     if ParallelPort is None:
-        logger.warning("psychopy.parallel has been imported but no "
-                       "parallel port driver found. Install either "
-                       "inpout32, inpoutx64 or dlportio")
+        raise RuntimeError("psychopy.parallel has been imported but no "
+                           "parallel port driver found. Install either "
+                           "inpout32, inpoutx64 or dlportio.")
 else:
-    logger.warning("psychopy.parallel has been imported on a Mac "
-                   "(which doesn't have a parallel port?)")
-
-    # macOS doesn't have a parallel port but write the class for doc purps
-    class ParallelPort:
-        """Class for read/write access to the parallel port on Windows & Linux
-
-        Usage::
-
-            from psychopy import parallel
-            port = parallel.ParallelPort(address=0x0378)
-
-            port.setData(4)
-            port.readPin(2)
-            port.setPin(2, 1)
-        """
-
-        def __init__(self, address):
-            """This is just a dummy constructor to avoid errors
-            when the parallel port cannot be initiated
-            """
-            msg = ("psychopy.parallel has been imported but (1) no parallel "
-                   "port driver could be found or accessed on Windows or "
-                   "(2) PsychoPy is run on a Mac (without parallel-port "
-                   "support for now)")
-            logger.warning(msg)
-
-        def setData(self, data):
-            """Set the data to be presented on the parallel port (one ubyte).
-            Alternatively you can set the value of each pin (data pins are
-            pins 2-9 inclusive) using :func:`~psychopy.parallel.setPin`
-
-            Examples::
-
-                from psychopy import parallel
-                port = parallel.ParallelPort(address=0x0378)
-
-                port.setData(0)  # sets all pins low
-                port.setData(255)  # sets all pins high
-                port.setData(2)  # sets just pin 3 high (pin2 = bit0)
-                port.setData(3)  # sets just pins 2 and 3 high
-
-            You can also convert base 2 to int easily in python::
-
-                port.setData( int("00000011", 2) )  # pins 2 and 3 high
-                port.setData( int("00000101", 2) )  # pins 2 and 4 high
-            """
-            sys.stdout.flush()
-            raise NotImplementedError("Parallel ports don't work on a Mac")
-
-        def readData(self):
-            """Return the value currently set on the data pins (2-9)
-            """
-            raise NotImplementedError("Parallel ports don't work on a Mac")
-
-        def readPin(self, pinNumber):
-            """Determine whether a desired (input) pin is high(1) or low(0).
-
-            Pins 2-13 and 15 are currently read here
-            """
-            raise NotImplementedError("Parallel ports don't work on a Mac")
+    raise RuntimeError("psychopy.parallel has been imported on a Mac "
+                       "(which doesn't have a parallel port?)")
