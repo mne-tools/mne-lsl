8a1918fb76ac3c7b08ff00e48f36257ef4ead2b9
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2022-12-07 11:10:22 +0100

    take a step back with working version

--- bsl/lsl/stream_inlet.py
+++ bsl/lsl/stream_inlet.py
@@ -18,6 +18,212 @@ from .utils import _free_char_p_array_memory, handle_error
 
 
 class StreamInlet:
+
+    def __init__(self, sinfo, max_buflen=360, max_chunklen=0, recover=True, processing_flags=0):
+        _check_type(sinfo, (_BaseStreamInfo,), "sinfo")
+        self.obj = lib.lsl_create_inlet(
+            sinfo.obj, max_buflen, max_chunklen, recover
+        )
+        self.obj = c_void_p(self.obj)
+        if not self.obj:
+            raise RuntimeError("The StreamInlet could not be created.")
+
+        if processing_flags > 0:
+            handle_error(lib.lsl_set_postprocessing(self.obj, processing_flags))
+
+        # properties from the StreamInfo
+        self._channel_format = sinfo._channel_format
+        self._name = sinfo.name
+        self._n_channels = sinfo.n_channels
+        self._sfreq = sinfo.sfreq
+        self._stype = sinfo.stype
+
+        # inlet properties
+        self.do_pull_sample = fmt2pull_sample[self._channel_format]
+        self.do_pull_chunk = fmt2pull_chunk[self._channel_format]
+        self.value_type = fmt2type[self._channel_format]
+        self.sample_type = self.value_type * self._n_channels
+        self.sample = self.sample_type()
+        self.buffers = {}
+
+    def __del__(self):
+        # noinspection PyBroadException
+        try:
+            lib.lsl_destroy_inlet(self.obj)
+        except:
+            pass
+
+    def info(self, timeout=320000):
+        errcode = c_int()
+        result = lib.lsl_get_fullinfo(self.obj, c_double(timeout),
+                                      byref(errcode))
+        handle_error(errcode)
+        return _BaseStreamInfo(result)
+
+    def open_stream(self, timeout=320000):
+        errcode = c_int()
+        lib.lsl_open_stream(self.obj, c_double(timeout), byref(errcode))
+        handle_error(errcode)
+
+    def close_stream(self):
+        lib.lsl_close_stream(self.obj)
+
+    def time_correction(self, timeout=320000):
+        errcode = c_int()
+        result = lib.lsl_time_correction(self.obj, c_double(timeout),
+                                         byref(errcode))
+        handle_error(errcode)
+        return result
+
+    def pull_sample(self, timeout=320000, sample=None):
+        # support for the legacy API
+        if type(timeout) is list:
+            assign_to = timeout
+            timeout = sample if type(sample) is float else 0.0
+        else:
+            assign_to = None
+
+        errcode = c_int()
+        timestamp = self.do_pull_sample(self.obj, byref(self.sample),
+                                        self._n_channels, c_double(timeout),
+                                        byref(errcode))
+        handle_error(errcode)
+        if timestamp:
+            sample = [v for v in self.sample]
+            if self._channel_format == cf_string:
+                sample = [v.decode('utf-8') for v in sample]
+            if assign_to is not None:
+                assign_to[:] = sample
+            return sample, timestamp
+        else:
+            return None, None
+
+    def pull_chunk(self, timeout=0.0, n_samples=1024, dest_obj=None):
+        # look up a pre-allocated buffer of appropriate length
+        num_channels = self._n_channels
+        max_values = n_samples * num_channels
+
+        if n_samples not in self.buffers:
+            # noinspection PyCallingNonCallable
+            self.buffers[n_samples] = ((self.value_type * max_values)(),
+                                         (c_double * n_samples)())
+        if dest_obj is not None:
+            data_buff = (self.value_type * max_values).from_buffer(dest_obj)
+        else:
+            data_buff = self.buffers[n_samples][0]
+        ts_buff = self.buffers[n_samples][1]
+
+        # read data into it
+        errcode = c_int()
+        # noinspection PyCallingNonCallable
+        num_elements = self.do_pull_chunk(self.obj, byref(data_buff),
+                                          byref(ts_buff), max_values,
+                                          n_samples, c_double(timeout),
+                                          byref(errcode))
+        handle_error(errcode)
+        # return results (note: could offer a more efficient format in the
+        # future, e.g., a numpy array)
+        num_samples = num_elements / num_channels
+        if dest_obj is None:
+            samples = [[data_buff[s * num_channels + c] for c in range(num_channels)]
+                       for s in range(int(num_samples))]
+            if self._channel_format == cf_string:
+                samples = [[v.decode('utf-8') for v in s] for s in samples]
+                _free_char_p_array_memory(data_buff, max_values)
+        else:
+            samples = None
+        timestamps = [ts_buff[s] for s in range(int(num_samples))]
+        return samples, timestamps
+
+    def samples_available(self):
+        return lib.lsl_samples_available(self.obj)
+
+    def flush(self):
+        return lib.lsl_inlet_flush(self.obj)
+
+    def was_clock_reset(self):
+        return bool(lib.lsl_was_clock_reset(self.obj))
+
+    # -------------------------------------------------------------------------
+    @copy_doc(_BaseStreamInfo.channel_format)
+    @property
+    def channel_format(self) -> str:
+        return fmt2string[self._channel_format]
+
+    @copy_doc(_BaseStreamInfo.n_channels)
+    @property
+    def n_channels(self) -> int:
+        return self._n_channels
+
+    @copy_doc(_BaseStreamInfo.name)
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @copy_doc(_BaseStreamInfo.sfreq)
+    @property
+    def sfreq(self) -> float:
+        return self._sfreq
+
+    @copy_doc(_BaseStreamInfo.stype)
+    @property
+    def stype(self) -> str:
+        return self._stype
+
+    # -------------------------------------------------------------------------
+    def get_sinfo(self, timeout: Optional[float] = None) -> _BaseStreamInfo:
+        """`~bsl.lsl.StreamInfo` corresponding to this Inlet.
+
+        Parameters
+        ----------
+        timeout : float | None
+            Optional timeout (in seconds) of the operation. By default, timeout
+            is disabled.
+        """
+        timeout = StreamInlet._check_timeout(timeout)
+        errcode = c_int()
+        result = lib.lsl_get_fullinfo(
+            self.obj, c_double(timeout), byref(errcode)
+        )
+        handle_error(errcode)
+        return _BaseStreamInfo(result)
+
+    # -------------------------------------------------------------------------
+    @staticmethod
+    def _check_timeout(timeout: Optional[float]) -> float:
+        """Check that the provided timeout is valid.
+
+        Parameters
+        ----------
+        timeout : float | None
+            Timeout (in seconds) or None to disable timeout.
+
+        Returns
+        -------
+        timeout : float
+            Timeout (in seconds). If None was provided, a very large float is
+            provided.
+        """
+        # with _check_type, the execution takes 800-900 ns.
+        # with the try/except below, the execution takes 110 ns.
+        if timeout is None:
+            return 32000000.0  # about 1 year
+        try:
+            raise_ = timeout < 0
+        except Exception:
+            raise TypeError(
+                "The argument 'timeout' must be a strictly positive number."
+            )
+        if raise_:
+            raise ValueError(
+                "The argument 'timeout' must be a strictly positive number. "
+                f"{timeout} is invalid."
+            )
+        return timeout
+
+
+
+class NewStreamInlet:
     """An inlet to retrieve data and metadata on the network.
 
     Parameters
--- bsl/lsl/stream_outlet.py
+++ bsl/lsl/stream_outlet.py
@@ -19,6 +19,126 @@ from .utils import handle_error
 
 
 class StreamOutlet:
+
+    def __init__(self, sinfo, chunk_size=0, max_buffered=360):
+        _check_type(sinfo, (_BaseStreamInfo,), "sinfo")
+        _check_type(chunk_size, ("numeric",), "chunk_size")
+        if chunk_size < 1:
+            raise ValueError(
+                "The argument 'chunk_size' must contain a positive number. "
+                f"{chunk_size} is invalid."
+            )
+        _check_type(max_buffered, ("numeric",), "max_buffered")
+        if max_buffered < 0:
+            raise ValueError(
+                "The argument 'max_buffered' must contain a positive number. "
+                f"{max_buffered} is invalid."
+            )
+        self.obj = lib.lsl_create_outlet(sinfo.obj, chunk_size, max_buffered)
+        self.obj = c_void_p(self.obj)
+        if not self.obj:
+            raise RuntimeError("The StreamOutlet could not be created.")
+
+        # properties from the StreamInfo
+        self._channel_format = sinfo._channel_format
+        self._name = sinfo.name
+        self._n_channels = sinfo.n_channels
+        self._sfreq = sinfo.sfreq
+        self._stype = sinfo.stype
+
+        # outlet properties
+        self.do_push_sample = fmt2push_sample[self._channel_format]
+        self.do_push_chunk = fmt2push_chunk[self._channel_format]
+        self.value_type = fmt2type[self._channel_format]
+        self.sample_type = self.value_type * self._n_channels
+
+    def __del__(self):
+        # noinspection PyBroadException
+        try:
+            lib.lsl_destroy_outlet(self.obj)
+        except:
+            pass
+
+    def push_sample(self, x, timestamp=0.0, pushthrough=True):
+        if len(x) == self._n_channels:
+            if self._channel_format == cf_string:
+                x = [v.encode('utf-8') for v in x]
+            handle_error(self.do_push_sample(self.obj, self.sample_type(*x),
+                                             c_double(timestamp),
+                                             c_int(pushthrough)))
+        else:
+            raise ValueError("length of the sample (" + str(len(x)) + ") must "
+                             "correspond to the stream's channel count ("
+                             + str(self._n_channels) + ").")
+
+    def push_chunk(self, x, timestamp=0.0, pushthrough=True):
+        try:
+            n_values = self._n_channels * len(x)
+            data_buff = (self.value_type * n_values).from_buffer(x)
+            handle_error(self.do_push_chunk(self.obj, data_buff,
+                                            c_long(n_values),
+                                            c_double(timestamp),
+                                            c_int(pushthrough)))
+        except TypeError:
+            if len(x):
+                if type(x[0]) is list:
+                    x = [v for sample in x for v in sample]
+                if self._channel_format == cf_string:
+                    x = [v.encode('utf-8') for v in x]
+                if len(x) % self._n_channels == 0:
+                    constructor = self.value_type * len(x)
+                    # noinspection PyCallingNonCallable
+                    handle_error(self.do_push_chunk(self.obj, constructor(*x),
+                                                    c_long(len(x)),
+                                                    c_double(timestamp),
+                                                    c_int(pushthrough)))
+                else:
+                    raise ValueError("Each sample must have the same number of channels ("
+                                     + str(self._n_channels) + ").")
+
+    def have_consumers(self):
+        return bool(lib.lsl_have_consumers(self.obj))
+
+    def wait_for_consumers(self, timeout):
+        return bool(lib.lsl_wait_for_consumers(self.obj, c_double(timeout)))
+
+    def get_info(self):
+        outlet_info = lib.lsl_get_info(self.obj)
+        return _BaseStreamInfo(outlet_info)
+
+    # -------------------------------------------------------------------------
+    @copy_doc(_BaseStreamInfo.channel_format)
+    @property
+    def channel_format(self) -> str:
+        return fmt2string[self._channel_format]
+
+    @copy_doc(_BaseStreamInfo.n_channels)
+    @property
+    def n_channels(self) -> int:
+        return self._n_channels
+
+    @copy_doc(_BaseStreamInfo.name)
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @copy_doc(_BaseStreamInfo.sfreq)
+    @property
+    def sfreq(self) -> float:
+        return self._sfreq
+
+    @copy_doc(_BaseStreamInfo.stype)
+    @property
+    def stype(self) -> str:
+        return self._stype
+
+    # -------------------------------------------------------------------------
+    def get_sinfo(self) -> _BaseStreamInfo:
+        """`~bsl.lsl.StreamInfo` corresponding to this Outlet."""
+        return _BaseStreamInfo(lib.lsl_get_info(self.obj))
+
+
+class NewStreamOutlet:
     """An outlet to share data and metadata on the network.
 
     Parameters
