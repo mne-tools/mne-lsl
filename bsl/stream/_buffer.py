4ee675b3ad7d45a7798b07ffe089d4174a70262a
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-05-31 15:05:36 +0200

    add buffer update logic

--- bsl/stream/_buffer.py
+++ bsl/stream/_buffer.py
@@ -11,6 +11,8 @@ import numpy as np
 from bsl.lsl.constants import fmt2numpy
 
 if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
     from bsl.lsl import StreamInlet
 
 
@@ -21,6 +23,15 @@ class Buffer:
         self._buffer = np.zeros(
             ceil(bufsize * inlet.sfreq), inlet.n_channels, dtype=fmt2numpy[inlet._dtype]
         )
+        # count the number of samples that go through the buffer
+        self._samples_retrieved = 0
 
-    def update(self, data):
-        self._buffer = np.roll(self._buffer, data.size)
+    def update(self, data: NDArray[float]):
+        # data is a pull from an inlet, thus the shape is (n_samples, n_channels)
+        self._buffer = np.roll(self._buffer, -data.shape[0], axis=0)
+        self._buffer[-data.shape[0]:, :] = data
+        self._samples_retrieved += data.shape[0]
+
+    @property
+    def samples_retrieved(self) -> int:
+        return self._samples_retrieved
