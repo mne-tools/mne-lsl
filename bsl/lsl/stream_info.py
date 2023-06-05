85b61fc6b22ac9f11ead3b8e204f48cc0531e1ff
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-06-05 14:48:50 +0200

    get channel setter info working

--- bsl/lsl/stream_info.py
+++ bsl/lsl/stream_info.py
@@ -259,7 +259,7 @@ class _BaseStreamInfo:
     def get_channel_units(self) -> Optional[List[str]]:
         pass
 
-    def set_channel_names(self, ch_names: Union[List[str], Tuple[str]]):
+    def set_channel_names(self, ch_names: Union[List[str], Tuple[str]]) -> None:
         """Set the channel names in the description. Existing labels are overwritten.
 
         Parameters
@@ -267,39 +267,78 @@ class _BaseStreamInfo:
         ch_names : sequence of str
             List of channel names, matching the number of total channels.
         """
-        check_type(ch_names, (list, tuple), "ch_names")
-        for ch_name in ch_names:
-            check_type(ch_name, (str,), "ch_name")
-        if len(ch_names) != self.n_channels:
+        self._set_channel_info(ch_names, "ch_names")
+
+    def set_channel_types(self, ch_types: Union[str, List[str]]) -> None:
+        """Set the channel types in the description. Existing types are overwritten.
+
+        The types are given as human readable strings, e.g. ``'eeg'``.
+
+        Parameters
+        ----------
+        ch_types : sequence of str | str
+            List of channel types, matching the number of total channels.
+            If a single `str` is provided, the type is applied to all channels.
+        """
+        ch_types = (
+            [ch_types] * self.n_channels if isinstance(ch_types, str) else ch_types
+        )
+        self._set_channel_info(ch_types, "ch_types")
+
+    def set_channel_units(self, ch_units: Union[str, List[str]]) -> None:
+        """Set the channel units in the description. Existing units are overwritten.
+
+        The units are given as human readable strings, e.g. ``'microvolts'``.
+
+        Parameters
+        ----------
+        ch_units : sequence of str | str
+            List of channel units, matching the number of total channels.
+            If a single `str` is provided, the unit is applied to all channels.
+        """
+        ch_units = (
+            [ch_units] * self.n_channels if isinstance(ch_units, str) else ch_units
+        )
+        self._set_channel_info(ch_units, "ch_units")
+
+    def _set_channel_info(self, ch_infos: List[str], name: str) -> None:
+        """Set the 'channel/name' element in the XML tree."""
+        check_type(ch_infos, (list, tuple), name)
+        for ch_info in ch_infos:
+            check_type(ch_info, (str,), name.rstrip("s"))
+        if len(ch_infos) != self.n_channels:
             raise ValueError(
-                f"The number of provided channel names {len(ch_names)} must match the "
+                f"The number of provided channel {name} {len(ch_infos)} must match the "
                 f"number of channels {self.n_channels}."
             )
 
-        channels = self.desc.append_child("channels")
-        for ch_name in ch_names:
-            ch = channels.append_child("channel")
-            ch.append_child_value("label", ch_name)
+        if self.desc.child("channels").empty():
+            channels = self.desc.append_child("channels")
+        else:
+            channels = self.desc.child("channels")
 
-    def set_channel_types(self, ch_types: List[str]):
-        """Set the channel types in the description. Existing types are overwritten.
+        # fill the 'channel/name' element of the tree and overwrite existing values
+        mapping_lsl = {
+            "ch_names": "label",
+            "ch_types": "type",
+            "ch_units": "unit",
+        }
+        ch = channels.child("channel")
+        for ch_info in ch_infos:
+            if ch.empty():
+                ch = channels.append_child("channel")
 
-        Parameters
-        ----------
-        ch_types : sequence of str
-            List of channel types, matching the number of total channels.
-        """
-        pass
+            if ch.child(mapping_lsl[name]).empty():
+                ch.append_child_value(mapping_lsl[name], ch_info)
+            else:
+                ch.child(mapping_lsl[name]).first_child().set_value(ch_info)
+            ch = ch.next_sibling()
 
-    def set_channel_units(self, ch_units: List[str]):
-        """Set the channel units in the description. Existing units are overwritten.
-
-        Parameters
-        ----------
-        ch_units : sequence of str
-            List of channel units, matching the number of total channels.
-        """
-        pass
+        # in case the original sinfo had more 'channel' than the correct number
+        while not ch.empty():
+            ch_next = ch.next_sibling()
+            channels.remove_child(ch)
+            ch = ch_next
 
 
 class StreamInfo(_BaseStreamInfo):
