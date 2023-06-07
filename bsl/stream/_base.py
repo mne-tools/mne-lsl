a467f97a4112aa05521aca146a2c91b5c8f520df
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-06-07 17:28:08 +0200

    keep improving structure

--- bsl/stream/_base.py
+++ bsl/stream/_base.py
@@ -1,11 +1,24 @@
-from typing import List, Optional, Union
+# postponed evaluation of annotations, c.f. PEP 563 and PEP 649 alternatively, the type
+# hints can be defined as strings which will be evaluated with eval() prior to type
+# checking.
+from __future__ import annotations
 
-from mne import Info
+from math import ceil
+from threading import Timer
+from typing import TYPE_CHECKING
+
+import numpy as np
 
 from .. import logger
 from ..lsl import StreamInlet, resolve_streams
+from ..lsl.constants import fmt2numpy
 from ..utils._checks import check_type
 from ..utils.meas_info import create_info
+
+if TYPE_CHECKING:
+    from typing import List, Optional, Tuple, Union
+
+    from numpy.typing import NDarray
 
 
 class BaseStream:
@@ -16,22 +29,34 @@ class BaseStream:
         stype: Optional[str] = None,
         source_id: Optional[str] = None,
     ):
+        check_type(bufsize, ("numeric",), "bufsize")
+        if 0 <= bufsize:
+            raise ValueError(
+                "The buffer size 'bufsize' must be a strictly positive number. "
+                f"{bufsize} is invalid."
+            )
         check_type(name, (str, None), "name")
         check_type(stype, (str, None), "stype")
         check_type(source_id, (str, None), "source_id")
         self._name = name
         self._stype = stype
         self._source_id = source_id
+        self._bufsize = bufsize
 
-        # variables defined after resolution
+        # -- variables defined after resolution ----------------------------------------
         self._sinfo = None
         self._info = None
 
-        # variables defined after connection
+        # -- variables defined after connection ----------------------------------------
         self._inlet = None
-        self._connected = False
+        # The buffer shape is similar to a pull_sample/pull_chunk from an inlet:
+        # (n_samples, n_channels). New samples are added to the right of the buffer
+        # while old samples are removed from the left of the buffer.
+        self._buffer = None
+        # picks defines the selected channels and their order
+        self._picks = None
 
-    def resolve(self, timeout: float = 1.0):
+    def resolve(self, timeout: float = 10) -> None:
         sinfos = resolve_streams(timeout, self._name, self._stype, self._source_id)
         if len(sinfos) != 1:
             raise RuntimeError(
@@ -53,7 +78,8 @@ class BaseStream:
         self,
         processing_flags: Optional[Union[str, List[str]]] = None,
         timeout: Optional[float] = 10,
-    ):
+        ufreq: float = 5,
+    ) -> None:
         """Connect to the LSL stream and initiate data collection in the buffer.
 
         Parameters
@@ -71,6 +97,9 @@ class BaseStream:
         timeout : float | None
             Optional timeout (in seconds) of the operation. ``None`` disables the
             timeout.
+        ufreq : float
+            Update frequency (Hz) at which chunks of data are pulled from the
+            `~bsl.lsl.StreamInlet`.
         """
         # The threadsafe processing flag should not be needed for this class. If it is
         # provided, then it means the user is retrieving and doing something with the
@@ -83,17 +112,56 @@ class BaseStream:
                 "separate thread, please instantiate the StreamInlet directly from "
                 "bsl.lsl.StreamInlet."
             )
+        check_type(ufreq, ("numeric",), "ufreq")
+        if ufreq <= 0:
+            raise ValueError(
+                "The update frequency 'ufreq' must be a strictly positive number "
+                "defining the frequency at which new samples are acquired in Hz. For "
+                "instance, 5 Hz corresponds to a pull every 200 ms. The provided "
+                f"{ufreq} is invalid."
+            )
         self._inlet = StreamInlet(self._sinfo, processing_flags=processing_flags)
         self._inlet.open_stream(timeout=timeout)
         # initiate time-correction
         tc = self._inlet.time_correction(timeout=timeout)
         logger.info("The estimated timestamp offset is %.2f seconds.", tc)
 
+        # create buffer of shape (n_samples, n_channels) and (n_samples,)
+        self._buffer = np.zeros(
+            ceil(self._bufsize * self._inlet.sfreq),
+            self._inlet.n_channels,
+            dtype=fmt2numpy[self._inlet._dtype],
+        )
+        self._timestamps = np.zeros(
+            ceil(self._bufsize * self._inlet.sfreq), dtype=np.float64
+        )
+        self._picks = np.arange(0, self._inlet.n_channels)
+
+        # define the acquisition thread
+        self._update_delay = 1. / ufreq
+        self._update_thread = Timer(1 / self._update_delay, self._update)
+        self._update_thread.start()
+
     def disconnect(self):
         pass
 
-    def get_data(self, winsize: float):
-        pass
+    def _update(self) -> None:
+        """Update function pulling new samples in the buffer at a regular interval."""
+        data, timestamps = self._inlet.pull_chunk(timeout=0.0)
+        if timestamps.size != 0:
+            self._buffer = np.roll(self._buffer, -data.shape[0], axis=0)
+            self._timestamps = np.roll(self._timestamps, -timestamps.size, axis=0)
+            self._buffer[-data.shape[0] :, :] = data
+            self._timestamps[-timestamps.size:] = timestamps
+
+        # recreate the timer thread as it is one-call only
+        self._update_thread = Timer(self._update_delay, self._update)
+        self._update_thread.start()
+
+    def get_data(self, winsize: float) -> Tuple[NDarray[float], NDarray[float]]:
+        assert 0 <= winsize, "The window size must be a strictly positive number."
+        n_samples = ceil(winsize * self._inlet.sfreq)
+        return self._buffer[-n_samples:, self._picks], self._timestamps[-n_samples:]
 
     def set_channel_types(self):
         pass
