2250a49a809c5df69c598c905784d91411cc6b65
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-08-28 22:29:30 +0200

    set daemon to True

--- bsl/player.py
+++ bsl/player.py
@@ -74,7 +74,7 @@ class Player(ContainsMixin):
         """Start streaming data on the LSL `~bsl.lsl.StreamOutlet`."""
         self._outlet = StreamOutlet(self._sinfo, self._chunk_size)
         self._streaming_delay = self.chunk_size / self.info["sfreq"]
-        self._streaming_thread = Timer(self._streaming_delay, self._stream)
+        self._streaming_thread = Timer(self._streaming_delay, self._stream, daemon=True)
         self._target_timestamp = local_clock() + self._streaming_delay
         self._streaming_thread.start()
 
@@ -113,7 +113,7 @@ class Player(ContainsMixin):
         delay = self._streaming_delay + delta
 
         # recreate the timer thread as it is one-call only
-        self._streaming_thread = Timer(delay, self._stream)
+        self._streaming_thread = Timer(delay, self._stream, daemon=True)
         self._streaming_thread.start()
 
         # retrieve data and push to the stream outlet
@@ -136,6 +136,10 @@ class Player(ContainsMixin):
     # ----------------------------------------------------------------------------------
     def __del__(self):
         """Delete the player and destroy the `~bsl.lsl.StreamOutlet`."""
+        try:
+            self.stop()
+        except Exception:
+            pass
         try:
             del self._outlet
         except Exception:
