61228d393faa1da0e823f1e929e329f6e91490db
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-08-31 17:29:36 +0200

    Remove old utilities (#102)

--- bsl/__init__.py
+++ bsl/__init__.py
@@ -4,6 +4,5 @@ from . import datasets  # noqa: F401
 from ._version import __version__  # noqa: F401
 from .player import Player  # noqa: F401
 from .stream import Stream  # noqa: F401
-from .stream_viewer import StreamViewer  # noqa: F401
 from .utils.config import sys_info  # noqa: F401
 from .utils.logs import add_file_handler, logger, set_log_level  # noqa: F401
