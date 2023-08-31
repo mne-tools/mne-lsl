cd463d4f981c9015b2b632597d4075cd5bb35cee
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-08-31 17:13:33 +0200

    Remove deprecated datasets and TriggerDef (#101)
    Former-commit-id: acbcf92310cf88008abbeec1a373f34ab75d717b

--- bsl/triggers/__init__.py
+++ bsl/triggers/__init__.py
@@ -1,11 +1,9 @@
 from .lsl import LSLTrigger
 from .mock import MockTrigger
 from .parallel import ParallelPortTrigger
-from .trigger_def import TriggerDef
 
 __all__ = [
     "LSLTrigger",
     "MockTrigger",
     "ParallelPortTrigger",
-    "TriggerDef",
 ]
