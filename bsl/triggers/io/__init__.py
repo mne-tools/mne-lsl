50b4cbb55cdd65bd9c42c0b35e90d9ad68e039fd
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-09-01 13:18:48 +0200

    [MNT] clean-up __init__.py files

--- bsl/triggers/io/__init__.py
+++ bsl/triggers/io/__init__.py
@@ -1,30 +1,27 @@
 """
-This module provides read / write access to the parallel port for
-Linux or Windows.
+This module provides read / write access to the parallel port for Linux or Windows.
 
-The :class:`~psychopy.parallel.Parallel` class described below will
-attempt to load whichever parallel port driver is first found on your
-system and should suffice in most instances. If you need to use a specific
-driver then, instead of using :class:`~psychopy.parallel.ParallelPort`
-shown below you can use one of the following as drop-in replacements,
-forcing the use of a specific driver:
+The :class:`~psychopy.parallel.Parallel` class described below will attempt to load
+whichever parallel port driver is first found on your system and should suffice in most
+instances. If you need to use a specific driver then, instead of using
+:class:`~psychopy.parallel.ParallelPort` shown below you can use one of the following as
+drop-in replacements, forcing the use of a specific driver:
 
     - `psychopy.parallel.PParallelInpOut`
     - `psychopy.parallel.PParallelDLPortIO`
     - `psychopy.parallel.PParallelLinux`
 
-Either way, each instance of the class can provide access to a different
-parallel port.
+Either way, each instance of the class can provide access to a different parallel port.
 
 This code snippet is inspired from the parallel module of PsychoPy.
 """
 
 import sys
 
-# To make life easier, only try drivers which have a hope in heck of working.
-# Because hasattr() in connection to windll ends up in an OSError trying to
-# load 32bit drivers in a 64bit environment, different drivers defined in
-# the dictionary 'drivers' are tested.
+# To make life easier, only try drivers which have a hope in heck of working. Because
+# hasattr() in connection to windll ends up in an OSError trying to load 32bit drivers
+# in a 64bit environment, different drivers defined in the dictionary 'drivers' are
+# tested.
 
 if sys.platform.startswith("linux"):
     from ._linux import PParallelLinux
