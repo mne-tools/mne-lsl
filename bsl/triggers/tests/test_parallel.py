9b9b0d6c0ffba727181a8b521cb9ddc37833292d
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-09-11 17:43:01 +0200

    Improve parallel port type inference (#124)
    Former-commit-id: 57433c066e419c44fc7e26b78be6642989ad9248

--- bsl/triggers/tests/test_parallel.py
+++ bsl/triggers/tests/test_parallel.py
@@ -5,24 +5,37 @@ import pytest
 from bsl.triggers import ParallelPortTrigger
 
 
-def test_infer_port_type():
-    """Test port type inference patterns."""
+@pytest.mark.skipif(system() != "Linux", reason="requires Linux")
+def test_infer_port_type_linux():
+    """Test port type inference patterns on linux."""
     assert ParallelPortTrigger._infer_port_type("arduino") == "arduino"
+    assert ParallelPortTrigger._infer_port_type("/dev/parport0") == "pport"
+    assert ParallelPortTrigger._infer_port_type("/dev/parport1") == "pport"
+    assert ParallelPortTrigger._infer_port_type("/dev/ttyACM0") == "arduino"
+    assert ParallelPortTrigger._infer_port_type("/dev/ttyACM1") == "arduino"
+    with pytest.raises(RuntimeError, match="provide the 'port_type' argument"):
+        ParallelPortTrigger._infer_port_type("101")
+    with pytest.raises(TypeError, match="provided as a string"):
+        ParallelPortTrigger._infer_port_type(0x4FB8)
 
-    if system() == "Linux":
-        assert ParallelPortTrigger._infer_port_type("/dev/parport0") == "pport"
-        assert ParallelPortTrigger._infer_port_type("/dev/parport1") == "pport"
-        assert ParallelPortTrigger._infer_port_type("/dev/ttyACM0") == "arduino"
-        assert ParallelPortTrigger._infer_port_type("/dev/ttyACM1") == "arduino"
-        with pytest.raises(RuntimeError, match="provide the 'port_type' argument"):
-            ParallelPortTrigger._infer_port_type("101")
-        with pytest.raises(RuntimeError, match="for parallel ports on windows."):
-            ParallelPortTrigger._infer_port_type(0x4FB8)
 
-    elif system() == "Windows":
-        assert ParallelPortTrigger._infer_port_type("COM7") == "arduino"
-        assert ParallelPortTrigger._infer_port_type("COM8") == "arduino"
-        assert ParallelPortTrigger._infer_port_type(0x4FB8) == "pport"
+@pytest.mark.skipif(system() != "Windows", reason="requires Windows")
+def test_infer_port_type_windows():
+    """Test port type inference patterns on Windows."""
+    assert ParallelPortTrigger._infer_port_type("arduino") == "arduino"
+    assert ParallelPortTrigger._infer_port_type("COM7") == "arduino"
+    assert ParallelPortTrigger._infer_port_type("COM8") == "arduino"
+    assert ParallelPortTrigger._infer_port_type(0x4FB8) == "pport"
+
+
+@pytest.mark.skipif(system() != "Darwin", reason="requires macOS")
+def test_infer_port_type_macos():
+    """Test port type inference patterns on Windows."""
+    assert ParallelPortTrigger._infer_port_type("arduino") == "arduino"
+    with pytest.raises(RuntimeError, match="macOS does not support"):
+        ParallelPortTrigger._infer_port_type("/dev/parport0")
+    with pytest.raises(RuntimeError, match="macOS does not support"):
+        ParallelPortTrigger._infer_port_type(0x4FB8)
 
 
 def test_search_arduino():
