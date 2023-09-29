78c2ca92b1843418a4bcdeab4b3f6afe5e49ba5c
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-09-29 20:50:53 +0200

    fix fixture

--- bsl/conftest.py
+++ bsl/conftest.py
@@ -1,6 +1,6 @@
 from pytest import fixture
 
-from bsl import Player
+from bsl.player import PlayerLSL
 from bsl.datasets import testing
 
 
@@ -8,5 +8,5 @@ from bsl.datasets import testing
 def mock_lsl_stream():
     """Create a mock LSL stream for testing."""
     fname = testing.data_path() / "sample-eeg-ant-raw.fif"
-    with Player(fname, "BSL-Player-pytest", chunk_size=16):
+    with PlayerLSL(fname, "BSL-Player-pytest", chunk_size=16):
         yield
