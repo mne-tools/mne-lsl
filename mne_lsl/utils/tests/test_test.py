47bd55f11d56588c093396f21bc84e19bfdbd6f2
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-10-07 12:51:45 +0200

    Add tests for info comparison function, add toml-sort to CIs, add python 3.12 to CIs (#153)
    Former-commit-id: 294a9e2fee9b1bf1a9573677ee82aa9548794195

--- mne_lsl/utils/tests/test_test.py
+++ mne_lsl/utils/tests/test_test.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pytest
-from mne.io import read_raw
+from mne.io import read_info, read_raw
 
 from mne_lsl.datasets import testing
-from mne_lsl.utils._tests import match_stream_and_raw_data
+from mne_lsl.utils._tests import compare_infos, match_stream_and_raw_data
 
-fname = testing.data_path() / "sample-eeg-ant-raw.fif"
-raw = read_raw(fname, preload=True)
+raw = read_raw(testing.data_path() / "sample-eeg-ant-raw.fif", preload=True)
+info = read_info(testing.data_path() / "sample_audvis_raw.fif")
 
 
 def test_match_stream_and_raw_data():
-    """Make sure the match works as intended."""
+    """Test that the data match works as intended."""
     # test default working match
     data = raw.get_data()  # (n_channels, n_samples)
     match_stream_and_raw_data(data[:, 10:100], raw)
@@ -37,3 +37,34 @@ def test_match_stream_and_raw_data():
     # shape error
     with pytest.raises(ValueError, match="operands could not be broadcast together"):
         match_stream_and_raw_data(data[:10, 10:100], raw)
+
+
+def test_compare_infos():
+    """Test that the partial info comparison works as intended."""
+    with pytest.raises(AssertionError):
+        compare_infos(info, raw.info)
+
+    info2 = info.copy()
+    compare_infos(info, info2)
+    with info2._unlock():
+        info2["projs"] = []
+    with pytest.raises(AssertionError):
+        compare_infos(info, info2)
+
+    info2 = info.copy()
+    compare_infos(info, info2)
+    with info2._unlock():
+        info2["dig"] = None
+    with pytest.raises(AssertionError):
+        compare_infos(info, info2)
+
+    for param, value in zip(
+        ("kind", "coil_type", "loc", "unit", "unit_mul", "ch_name", "coord_frame"),
+        (202, 1, np.ones(12), 107, -6, "101", 0),
+    ):
+        info2 = info.copy()
+        compare_infos(info, info2)
+        with info2._unlock():
+            info2["chs"][0][param] = value
+        with pytest.raises(AssertionError):
+            compare_infos(info, info2)
