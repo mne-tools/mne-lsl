72d08cc62c1bc1ef204561349be8c5fb72cf11b7
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-01-05 14:20:04 +0100

    add missing module imports

---
+++ mne_lsl/datasets/__init__.py
@@ -0,0 +1,2 @@
+from . import sample  # noqa: F401
+from . import testing  # noqa: F401
---
+++ mne_lsl/utils/__init__.py
@@ -0,0 +1,3 @@
+from . import config  # noqa: F401
+from . import logs  # noqa: F401
+from . import meas_info  # noqa: F401
