1a9171a11020ace904d67cca96096c4a8d59649f
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-03 13:48:12 +0200

    prevent logging tests from modifying log levels

--- mne_lsl/utils/tests/test_logs.py
+++ mne_lsl/utils/tests/test_logs.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from ..logs import add_file_handler, logger, set_log_level, verbose, warn
+from ..logs import _use_log_level, add_file_handler, logger, verbose, warn
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -14,27 +14,26 @@ if TYPE_CHECKING:
 
 def test_default_log_level(caplog: pytest.LogCaptureFixture):
     """Test the default log level."""
-    set_log_level("WARNING")  # set to default
+    with _use_log_level("WARNING"):  # set to default
+        caplog.clear()
+        logger.debug("101")
+        assert "101" not in caplog.text
 
-    caplog.clear()
-    logger.debug("101")
-    assert "101" not in caplog.text
+        caplog.clear()
+        logger.info("101")
+        assert "101" not in caplog.text
 
-    caplog.clear()
-    logger.info("101")
-    assert "101" not in caplog.text
+        caplog.clear()
+        logger.warning("101")
+        assert "101" in caplog.text
 
-    caplog.clear()
-    logger.warning("101")
-    assert "101" in caplog.text
+        caplog.clear()
+        logger.error("101")
+        assert "101" in caplog.text
 
-    caplog.clear()
-    logger.error("101")
-    assert "101" in caplog.text
-
-    caplog.clear()
-    logger.critical("101")
-    assert "101" in caplog.text
+        caplog.clear()
+        logger.critical("101")
+        assert "101" in caplog.text
 
 
 @pytest.mark.parametrize("level", ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"))
@@ -54,15 +53,14 @@ def test_logger(level: str, caplog: pytest.LogCaptureFixture):
         "ERROR": logging.ERROR,
         "CRITICAL": logging.CRITICAL,
     }
-    set_log_level(level)
-
-    for level_, function in level_functions.items():
-        caplog.clear()
-        function("101")
-        if level_int[level] <= level_int[level_]:
-            assert "101" in caplog.text
-        else:
-            assert "101" not in caplog.text
+    with _use_log_level(level):
+        for level_, function in level_functions.items():
+            caplog.clear()
+            function("101")
+            if level_int[level] <= level_int[level_]:
+                assert "101" in caplog.text
+            else:
+                assert "101" not in caplog.text
 
 
 def test_verbose(caplog: pytest.LogCaptureFixture):
@@ -76,10 +74,10 @@ def test_verbose(caplog: pytest.LogCaptureFixture):
 
     assert foo.__doc__ == "Foo function."
     assert foo.__name__ == "foo"
-    set_log_level("INFO")
-    caplog.clear()
-    foo()
-    assert "101" not in caplog.text
+    with _use_log_level("INFO"):
+        caplog.clear()
+        foo()
+        assert "101" not in caplog.text
 
     for level in (20, 25, 30, True, False, "WARNING", "ERROR"):
         caplog.clear()
@@ -89,7 +87,6 @@ def test_verbose(caplog: pytest.LogCaptureFixture):
     caplog.clear()
     foo(verbose="DEBUG")
     assert "101" in caplog.text
-    assert logger.level == logging.INFO
 
     # method
     class Foo:
@@ -106,32 +103,32 @@ def test_verbose(caplog: pytest.LogCaptureFixture):
             logger.debug("101")
 
     foo = Foo()
-    set_log_level("INFO")
-    caplog.clear()
-    foo.foo()
-    assert "101" not in caplog.text
-    caplog.clear()
-    foo.foo(verbose="DEBUG")
-    assert "101" in caplog.text
+    with _use_log_level("INFO"):
+        caplog.clear()
+        foo.foo()
+        assert "101" not in caplog.text
+        caplog.clear()
+        foo.foo(verbose="DEBUG")
+        assert "101" in caplog.text
 
-    # static method
-    caplog.clear()
-    Foo.foo2()
-    assert "101" not in caplog.text
-    caplog.clear()
-    Foo.foo2(verbose="DEBUG")
-    assert "101" in caplog.text
+        # static method
+        caplog.clear()
+        Foo.foo2()
+        assert "101" not in caplog.text
+        caplog.clear()
+        Foo.foo2(verbose="DEBUG")
+        assert "101" in caplog.text
 
 
 def test_file_handler(tmp_path: Path):
     """Test adding a file handler."""
     fname = tmp_path / "logs.txt"
     add_file_handler(fname)
-    set_log_level("WARNING")
-    logger.warning("test1")
-    logger.info("test2")
-    set_log_level("INFO")
-    logger.info("test3")
+    with _use_log_level("WARNING"):
+        logger.warning("test1")
+        logger.info("test2")
+    with _use_log_level("INFO"):
+        logger.info("test3")
     logger.handlers[-1].close()
     with open(fname) as file:
         lines = file.readlines()
@@ -144,17 +141,18 @@ def test_file_handler(tmp_path: Path):
 
 def test_warn(tmp_path: Path):
     """Test warning functions."""
-    set_log_level("ERROR")
-    warn("This is a warning.", RuntimeWarning)
-    set_log_level("WARNING")
-    with pytest.warns(RuntimeWarning, match="This is a warning."):
+    with _use_log_level("ERROR"):
+        warn("This is a warning.", RuntimeWarning)
+    with _use_log_level("WARNING"), pytest.warns(
+        RuntimeWarning, match="This is a warning."
+    ):
         warn("This is a warning.", RuntimeWarning)
     fname = tmp_path / "logs.txt"
     add_file_handler(fname)
     with pytest.warns(RuntimeWarning, match="Grrrrr"):
         warn("Grrrrr", RuntimeWarning)
-    set_log_level("ERROR")
-    warn("WoooW", RuntimeWarning)
+    with _use_log_level("ERROR"):
+        warn("WoooW", RuntimeWarning)
     logger.handlers[-1].close()
     with open(fname) as file:
         lines = file.readlines()
