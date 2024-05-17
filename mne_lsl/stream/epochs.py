22e4a0cd10dd28e467a0e444cc152e1d4bcae1c3
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-05-17 15:59:44 +0200

    better structure [ci skip]

---
+++ mne_lsl/stream/epochs.py
@@ -0,0 +1,368 @@
+from __future__ import annotations  # c.f. PEP 563, PEP 649
+
+from math import ceil
+from typing import TYPE_CHECKING
+
+import numpy as np
+from mne import pick_info
+from mne.utils import check_version
+
+if check_version("mne", "1.6"):
+    from mne._fiff.pick import _picks_to_idx
+elif check_version("mne", "1.5"):
+    from mne.io.pick import _picks_to_idx
+else:
+    from mne.io.pick import _picks_to_idx
+
+from ..utils._checks import check_type, ensure_int
+from ..utils._docs import fill_doc
+from ..utils.logs import logger, warn
+from ._base import BaseStream
+
+if TYPE_CHECKING:
+    from typing import Optional, Union
+
+    from mne import Info
+
+    from .._typing import ScalarIntArray
+
+
+@fill_doc
+class EpochsStream:
+    """Stream object representing a single real-time stream of epochs.
+
+    Note that a stream of epochs is necessarily connected to a regularly sampled stream
+    of continuous data, from which epochs are extracted depending on an internal event
+    channel or to an external event stream.
+
+    Parameters
+    ----------
+    stream : ``Stream``
+        Stream object to connect to, from which the epochs are extracted. The stream
+        must ber regularly sampled.
+    event_channels : str | list of str
+        Channel(s) to monitor for incoming events. The event channel(s) must be part of
+        the connected Stream of of the ``event_stream`` if provided. See notes for
+        details.
+    event_stream : ``Stream`` | None
+        Source from which events should be retrieved. If provided, event channels in the
+        connected ``stream`` are ignored in favor of the event channels in this separate
+        ``event_stream``. See notes for details.
+    event_id : int | str | dict
+        The ID of the events to consider from the event source. The event source can be
+        a channel from the connected Stream, in which case the event should be defined
+        as :class:`int`, or a separate event stream, in which case the event should be
+        defined either as :class:`int` or :class:`str`. If a :class:`dict` is provided,
+        it should map event names to event IDs. For example
+        ``dict(auditory=1, visual=2)``.
+    bufsize : int
+        Number of epochs to keep in the buffer.
+    %(epochs_tmin_tmax)s
+    %(baseline_epochs)s
+    %(picks_base)s all channels.
+    %(reject_epochs)s
+    %(flat)s
+    %(epochs_reject_tmin_tmax)s
+    detrend : int | str | None
+        The type of detrending to use. Can be ``'constant'`` or ``0`` for constant (DC)
+        detrend, ``'linear'`` or ``1`` for linear detrend, or ``None`` for no
+        detrending. Note that detrending is performed before baseline correction.
+
+    Notes
+    -----
+    Since events can be provided from multiple source, the arguments ``event_channels``,
+    ``event_source`` and ``event_id`` must work together to select which events should
+    be considered.
+
+    - if ``event_stream`` is ``None``, the events are extracted from channels within the
+      connected ``stream``. This ``stream`` is necessarily regularly sampled, thus the
+      event channels must correspond to MNE ``'stim'`` channels, i.e. channels on which
+      :func:`mne.find_events` can be applied.
+    - if ``event_stream`` is provided and is regularly sampled, the events are extracted
+      from channels in the ``event_stream``. The event channels must correspond to MNE
+      ``'stim'`` channels, i.e. channels on which :func:`mne.find_events` can be
+      applied.
+    - if ``event_stream`` is provided and is irregularly sampled, the events are
+      extracted from channels in the ``event_stream``. If the stream ``dtype`` is
+      :class:`str`, the value within the channels are used as separate events. If the
+      stream ``dtype`` is ``numerical``, the value within the channels are ignored and
+      the appearance of a new value in the stream is considered as a new event named
+      after the channel name. This last case can be useful when working with a
+      ``Player`` replaying annotations from a file as one-hot encoded events.
+
+    .. note::
+
+        In the 2 last cases where ``event_stream`` is provided, all channels in the
+        connected ``stream`` are ignored.
+    """
+
+    def __init__(
+        self,
+        stream: BaseStream,
+        event_channels: Union[str, list[str]],
+        event_stream: Optional[BaseStream],
+        event_id: Union[int, str, dict[str, Union[int, str]]],
+        bufsize: int,
+        tmin: float = -0.2,
+        tmax: float = 0.5,
+        baseline: Optional[tuple[Optional[float], Optional[float]]] = (None, 0),
+        picks: Optional[Union[str, list[str], int, list[int], ScalarIntArray]] = None,
+        reject: Optional[dict[str, float]] = None,
+        flat: Optional[dict[str, float]] = None,
+        reject_tmin: Optional[float] = None,
+        reject_tmax: Optional[float] = None,
+        detrend: Optional[Union[int, str]] = None,
+    ) -> None:
+        check_type(stream, (BaseStream,), "stream")
+        if not stream.connected and stream._info["sfreq"] != 0:
+            raise RuntimeError(
+                "The Stream must be a connected regularly sampled stream before "
+                "creating an EpochsStream."
+            )
+        self._stream = stream
+        check_type(tmin, ("numeric",), "tmin")
+        check_type(tmax, ("numeric",), "tmax")
+        if tmax <= tmin:
+            raise ValueError(
+                f"Argument 'tmax' (provided: {tmax}) must be greater than 'tmin' "
+                f"(provided: {tmin})."
+            )
+        # make sure the stream buffer is long enough to store an entire epoch, which is
+        # simpler than handling the case where the buffer is too short and we need to
+        # concatenate chunks to form a single epoch.
+        if stream._bufsize < tmax - tmin:
+            raise ValueError(
+                "The buffer size of the Stream must be at least as long as the epoch "
+                "duration (tmax - tmin)."
+            )
+        elif stream._bufsize < (tmax - tmin) * 1.2:
+            warn(
+                "The buffer size of the Stream is longer than the epoch duration, but "
+                "not by at least 20%. It is recommended to have a buffer size at least "
+                r"20% longer than the epoch duration to avoid data loss."
+            )
+        self._tmin = tmin
+        self._tmax = tmax
+        # check the event source(s)
+        check_type(event_stream, (BaseStream, None), "event_stream")
+        if event_stream is not None and not event_stream.connected:
+            raise RuntimeError(
+                "If 'event_stream' is provided, it must be connected before creating "
+                "an EpochsStream."
+            )
+        self._event_stream = event_stream
+        event_channels = (
+            [event_channels] if isinstance(event_channels, str) else event_channels
+        )
+        check_type(event_channels, (list,), "event_channels")
+        for elt in event_channels:
+            check_type(elt, (str,), "event_channels")
+            if self._event_stream is None and elt not in stream.ch_names:
+                raise ValueError(
+                    "The event channel(s) must be part of the connected Stream if an "
+                    f"'event_stream' is not provided. '{elt}' was not found."
+                )
+            elif self._event_stream is not None and elt not in event_stream.ch_names:
+                raise ValueError(
+                    "If 'event_stream' is provided, the event channel(s) must be part "
+                    f"of 'event_stream'. '{elt}' was not found."
+                )
+        self._event_channels = event_channels
+        # check and store the epochs general settings
+        self._bufsize = ensure_int(bufsize, "bufsize")
+        if self._bufsize <= 0:
+            raise ValueError(
+                "The buffer size, i.e. the number of epochs in the buffer, must be a "
+                "positive integer."
+            )
+        self._event_id = _ensure_event_id_dict(event_id)
+        _check_baseline(baseline)
+        self._baseline = baseline
+        _check_reject_flat(reject, flat, stream.info)
+        self._reject, self._flat = reject, flat
+        _check_reject_tmin_tmax(reject_tmin, reject_tmax, tmin, tmax)
+        self._reject_tmin, self._reject_tmax = reject_tmin, reject_tmax
+        self._detrend = _ensure_detrend_int(detrend)
+        # initialize the epoch buffer
+        self._picks = _picks_to_idx(
+            self._stream._info, picks, "all", "bads", allow_empty=False
+        )
+        self._info = pick_info(self._stream._info, self._picks)
+        self._buffer = np.zeros(
+            (
+                self._bufsize,
+                ceil((tmax - tmin) * self._info["sfreq"]),
+                self._picks.size,
+            ),
+            dtype=self._stream._buffer.dtype,
+        )
+        # mark the stream(s) as being epoched, which will prevent further channel
+        # modification and buffer size modifications.
+        self._stream._epoched += 1
+        if self._event_stream is not None:
+            self._event_stream._epoched += 1
+
+    def __del__(self) -> None:
+        """Delete the epoch stream object."""
+        logger.debug("Deleting %s", self)
+        self._stream._epoched -= 1
+        if self._event_stream is not None:
+            self._event_stream._epoched -= 1
+
+    def __repr__(self) -> str:  # pragma: no cover
+        """Representation of the instance."""
+
+    @property
+    def info(self) -> Info:
+        """Info of the epoched LSL stream.
+
+        :type: :class:`~mne.Info`
+        """
+        return self._info
+
+
+def _ensure_event_id_dict(
+    event_id: Union[int, str, dict[str, Union[int, str]]],
+) -> dict[str, Union[str, int]]:
+    """Ensure event_ids is a dictionary."""
+    check_type(event_id, (int, str, dict), "event_id")
+    raise_ = False
+    if isinstance(event_id, str):
+        if len(event_id) == 0:
+            raise_ = True
+        event_id = {event_id: event_id}
+    elif isinstance(event_id, int):
+        if event_id <= 0:
+            raise_ = True
+        event_id = {str(event_id): event_id}
+    else:
+        for key, value in event_id.items():
+            check_type(key, (str,), "event_id")
+            check_type(value, (int, str), "event_id")
+            if len(key) == 0:
+                raise_ = True
+            if (isinstance(value, str) and len(value) == 0) or (
+                isinstance(value, int) and value <= 0
+            ):
+                raise_ = True
+    if raise_:
+        raise ValueError(
+            "The 'event_id' must be a non-empty string, a positive integer or a "
+            "dictionary mapping non-empty strings to positive integers or non-empty "
+            "strings."
+        )
+    return event_id
+
+
+def _check_baseline(
+    baseline: Optional[tuple[Optional[float], Optional[float]]],
+    tmin: float,
+    tmax: float,
+) -> None:
+    """Check that the baseline is valid."""
+    check_type(baseline, (tuple, None), "baseline")
+    if baseline is None:
+        return
+    if len(baseline) != 2:
+        raise ValueError("The baseline must be a tuple of 2 elements.")
+    check_type(baseline[0], ("numeric", None), "baseline[0]")
+    check_type(baseline[1], ("numeric", None), "baseline[1]")
+    if baseline[0] is not None and baseline[0] < tmin:
+        raise ValueError(
+            "The beginning of the baseline period must be greater than or equal to "
+            "the beginning of the epoch period 'tmin'."
+        )
+    if baseline[1] is not None and tmax < baseline[1]:
+        raise ValueError(
+            "The end of the baseline period must be less than or equal to the end of "
+            "the epoch period 'tmax'."
+        )
+
+
+def _check_reject_flat(
+    reject: Optional[dict[str, float]], flat: Optional[dict[str, float]], info: Info
+) -> None:
+    """Check that the PTP rejection dictionaries are valid."""
+    check_type(reject, (dict, None), "reject")
+    check_type(flat, (dict, None), "flat")
+    ch_types = info.get_channel_types(unique=True)
+    if reject is not None:
+        for key, value in reject.items():
+            check_type(key, (str,), "reject")
+            check_type(value, ("numeric",), "reject")
+            if key not in ch_types:
+                raise ValueError(
+                    f"The channel type '{key}' in the rejection dictionary is not part "
+                    "of the connected Stream."
+                )
+            check_type(value, (float,), "reject")
+            if value <= 0:
+                raise ValueError(
+                    f"The peak-to-peak rejection value for channel type '{key}' must "
+                    "be a positive number."
+                )
+    if flat is not None:
+        for key, value in flat.items():
+            check_type(key, (str,), "flat")
+            check_type(value, ("numeric",), "flat")
+            if key not in ch_types:
+                raise ValueError(
+                    f"The channel type '{key}' in the flat rejection dictionary is not "
+                    "part of the connected Stream."
+                )
+            check_type(value, (float,), "flat")
+            if value <= 0:
+                raise ValueError(
+                    f"The flat rejection value for channel type '{key}' must be a "
+                    "positive number."
+                )
+
+
+def _check_reject_tmin_tmax(
+    reject_tmin: Optional[float], reject_tmax: Optional[float], tmin: float, tmax: float
+) -> None:
+    """Check that the rejection time window is valid."""
+    check_type(reject_tmin, ("numeric", None), "reject_tmin")
+    check_type(reject_tmax, ("numeric", None), "reject_tmax")
+    if reject_tmin is not None and reject_tmin < tmin:
+        raise ValueError(
+            "The beginning of the rejection time window must be greater than or equal "
+            "to the beginning of the epoch period 'tmin'."
+        )
+    if reject_tmax is not None and tmax < reject_tmax:
+        raise ValueError(
+            "The end of the rejection time window must be less than or equal to the "
+            "end of the epoch period 'tmax'."
+        )
+    if (
+        reject_tmin is not None
+        and reject_tmax is not None
+        and reject_tmax <= reject_tmin
+    ):
+        raise ValueError(
+            "The end of the rejection time window must be greater than the beginning "
+            "of the rejection time window."
+        )
+
+
+def _ensure_detrend_int(detrend: Optional[Union[int, str]]) -> Optional[int]:
+    """Ensure detrend is an integer."""
+    if detrend is None:
+        return None
+    if isinstance(detrend, str):
+        if detrend == "constant":
+            return 0
+        elif detrend == "linear":
+            return 1
+        else:
+            raise ValueError(
+                "The detrend argument must be 'constant', 'linear' or their integer "
+                "equivalent 0 and 1."
+            )
+    detrend = ensure_int(detrend, "detrend")
+    if detrend not in (0, 1):
+        raise ValueError(
+            "The detrend argument must be 'constant', 'linear' or their integer "
+            "equivalent 0 and 1."
+        )
+    return detrend
--- mne_lsl/stream/tests/test_epochs.py
+++ mne_lsl/stream/tests/test_epochs.py
@@ -1,67 +1,67 @@
 import pytest
 from mne import create_info
 
-from .._base import (
-    check_baseline,
-    check_reject_flat,
-    check_reject_tmin_tmax,
-    ensure_detrend_int,
-    ensure_event_id_dict,
+from ..epochs import (
+    _check_baseline,
+    _check_reject_flat,
+    _check_reject_tmin_tmax,
+    _ensure_detrend_int,
+    _ensure_event_id_dict,
 )
 
 
 def test_ensure_event_id_dict():
     """Test validation of event dictionary."""
-    assert ensure_event_id_dict(5) == {"5": 5}
-    assert ensure_event_id_dict({"5": 5}) == {"5": 5}
-    assert ensure_event_id_dict("test") == {"test": "test"}
-    assert ensure_event_id_dict({"test": "test"}) == {"test": "test"}
-    assert ensure_event_id_dict({"t": 5, "t2": "101"}) == {"t": 5, "t2": "101"}
+    assert _ensure_event_id_dict(5) == {"5": 5}
+    assert _ensure_event_id_dict({"5": 5}) == {"5": 5}
+    assert _ensure_event_id_dict("test") == {"test": "test"}
+    assert _ensure_event_id_dict({"test": "test"}) == {"test": "test"}
+    assert _ensure_event_id_dict({"t": 5, "t2": "101"}) == {"t": 5, "t2": "101"}
 
     with pytest.raises(ValueError, match="'event_id' must be a non-empty string"):
-        ensure_event_id_dict("")
+        _ensure_event_id_dict("")
     with pytest.raises(ValueError, match="'event_id' must be a non-empty string"):
-        ensure_event_id_dict(0)
+        _ensure_event_id_dict(0)
     with pytest.raises(ValueError, match="'event_id' must be a non-empty string"):
-        ensure_event_id_dict(-101)
+        _ensure_event_id_dict(-101)
     with pytest.raises(ValueError, match="'event_id' must be a non-empty string"):
-        ensure_event_id_dict({"5": 0})
+        _ensure_event_id_dict({"5": 0})
     with pytest.raises(ValueError, match="'event_id' must be a non-empty string"):
-        ensure_event_id_dict({"101": ""})
+        _ensure_event_id_dict({"101": ""})
 
     with pytest.raises(TypeError, match="must be an instance of"):
-        ensure_event_id_dict(5.5)
+        _ensure_event_id_dict(5.5)
     with pytest.raises(TypeError, match="must be an instance of"):
-        ensure_event_id_dict(None)
+        _ensure_event_id_dict(None)
     with pytest.raises(TypeError, match="must be an instance of"):
-        ensure_event_id_dict({"5": 5.5})
+        _ensure_event_id_dict({"5": 5.5})
     with pytest.raises(TypeError, match="must be an instance of"):
-        ensure_event_id_dict({"101": None})
+        _ensure_event_id_dict({"101": None})
     with pytest.raises(TypeError, match="must be an instance of"):
-        ensure_event_id_dict({5: "test"})
+        _ensure_event_id_dict({5: "test"})
 
 
 def test_check_baseline():
     """Test validation of the baseline."""
-    check_baseline(None, -0.2, 0.5)
-    check_baseline((None, None), -0.2, 0.5)
-    check_baseline((None, 0), -0.2, 0.5)
-    check_baseline((0, None), -0.2, 0.5)
-    check_baseline((0, 0), -0.2, 0.5)
+    _check_baseline(None, -0.2, 0.5)
+    _check_baseline((None, None), -0.2, 0.5)
+    _check_baseline((None, 0), -0.2, 0.5)
+    _check_baseline((0, None), -0.2, 0.5)
+    _check_baseline((0, 0), -0.2, 0.5)
 
     with pytest.raises(ValueError, match="The beginning of the baseline period must"):
-        check_baseline((-0.2, 0), -0.1, 0.5)
+        _check_baseline((-0.2, 0), -0.1, 0.5)
     with pytest.raises(ValueError, match="The end of the baseline period must"):
-        check_baseline((-0.2, 0.8), -0.2, 0.6)
+        _check_baseline((-0.2, 0.8), -0.2, 0.6)
 
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_baseline((-0.2, "test"), -0.2, 0.5)
+        _check_baseline((-0.2, "test"), -0.2, 0.5)
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_baseline(("test", 0.5), -0.2, 0.5)
+        _check_baseline(("test", 0.5), -0.2, 0.5)
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_baseline(("test", "test"), -0.2, 0.5)
+        _check_baseline(("test", "test"), -0.2, 0.5)
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_baseline(101, -0.2, 0.5)
+        _check_baseline(101, -0.2, 0.5)
 
 
 @pytest.fixture(scope="session")
@@ -74,61 +74,61 @@ def info():
 
 def test_check_reject_flat(info):
     """Test validation of the rejection dictionaries."""
-    check_reject_flat(None, None, info)
-    check_reject_flat(None, {"eeg": 1e-6}, info)
-    check_reject_flat({"eeg": 1e-6}, None, info)
-    check_reject_flat({"eeg": 1e-6}, {"eeg": 1e-6}, info)
+    _check_reject_flat(None, None, info)
+    _check_reject_flat(None, {"eeg": 1e-6}, info)
+    _check_reject_flat({"eeg": 1e-6}, None, info)
+    _check_reject_flat({"eeg": 1e-6}, {"eeg": 1e-6}, info)
 
     with pytest.raises(ValueError, match="peak-to-peak rejection value"):
-        check_reject_flat({"eeg": -1e-6}, None, info)
+        _check_reject_flat({"eeg": -1e-6}, None, info)
     with pytest.raises(ValueError, match="flat rejection value"):
-        check_reject_flat(None, {"eeg": -1e-6}, info)
+        _check_reject_flat(None, {"eeg": -1e-6}, info)
     with pytest.raises(ValueError, match="channel type '.*' in the .* is not part"):
-        check_reject_flat(None, {"eog": 1e-6}, info)
+        _check_reject_flat(None, {"eog": 1e-6}, info)
     with pytest.raises(ValueError, match="channel type '.*' in the .* is not part"):
-        check_reject_flat({"eog": 1e-6}, None, info)
+        _check_reject_flat({"eog": 1e-6}, None, info)
 
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_reject_flat(101, None, info)
+        _check_reject_flat(101, None, info)
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_reject_flat(None, 101, info)
+        _check_reject_flat(None, 101, info)
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_reject_flat({"eeg": "test"}, None, info)
+        _check_reject_flat({"eeg": "test"}, None, info)
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_reject_flat(None, {"eeg": "test"}, info)
+        _check_reject_flat(None, {"eeg": "test"}, info)
 
 
 def test_check_reject_tmin_tmax():
     """Test validatin of rejection time windows."""
-    check_reject_tmin_tmax(None, None, -0.2, 0.5)
-    check_reject_tmin_tmax(-0.2, 0.5, -0.2, 0.5)
-    check_reject_tmin_tmax(None, 0.5, -0.2, 0.5)
-    check_reject_tmin_tmax(-0.2, None, -0.2, 0.5)
-    check_reject_tmin_tmax(0, 0.1, -0.2, 0.5)
+    _check_reject_tmin_tmax(None, None, -0.2, 0.5)
+    _check_reject_tmin_tmax(-0.2, 0.5, -0.2, 0.5)
+    _check_reject_tmin_tmax(None, 0.5, -0.2, 0.5)
+    _check_reject_tmin_tmax(-0.2, None, -0.2, 0.5)
+    _check_reject_tmin_tmax(0, 0.1, -0.2, 0.5)
 
     with pytest.raises(ValueError, match="The beginning of the rejection time window"):
-        check_reject_tmin_tmax(-0.5, 0.5, -0.2, 0.5)
+        _check_reject_tmin_tmax(-0.5, 0.5, -0.2, 0.5)
     with pytest.raises(ValueError, match="end of the epoch period"):
-        check_reject_tmin_tmax(-0.2, 0.8, -0.2, 0.5)
+        _check_reject_tmin_tmax(-0.2, 0.8, -0.2, 0.5)
     with pytest.raises(ValueError, match="must be greater than the beginning"):
-        check_reject_tmin_tmax(0.5, -0.2, -0.2, 0.5)
+        _check_reject_tmin_tmax(0.5, -0.2, -0.2, 0.5)
 
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_reject_tmin_tmax("test", None, -0.2, 0.5)
+        _check_reject_tmin_tmax("test", None, -0.2, 0.5)
     with pytest.raises(TypeError, match="must be an instance of"):
-        check_reject_tmin_tmax(None, "test", -0.2, 0.5)
+        _check_reject_tmin_tmax(None, "test", -0.2, 0.5)
 
 
 def test_ensure_detrend_int():
     """Test validation of detrend."""
-    assert ensure_detrend_int(None) is None
-    assert ensure_detrend_int(0) == 0
-    assert ensure_detrend_int(1) == 1
-    assert ensure_detrend_int("constant") == 0
-    assert ensure_detrend_int("linear") == 1
+    assert _ensure_detrend_int(None) is None
+    assert _ensure_detrend_int(0) == 0
+    assert _ensure_detrend_int(1) == 1
+    assert _ensure_detrend_int("constant") == 0
+    assert _ensure_detrend_int("linear") == 1
 
     with pytest.raises(ValueError, match="detrend argument must be"):
-        ensure_detrend_int("test")
+        _ensure_detrend_int("test")
 
     with pytest.raises(TypeError, match="must be an integer"):
-        ensure_detrend_int(5.5)
+        _ensure_detrend_int(5.5)
