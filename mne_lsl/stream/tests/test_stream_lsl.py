d623d3b9e179810c17fcc01add8d11d13fb7b761
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-05-29 17:08:42 +0200

    fix flaky

--- mne_lsl/stream/tests/test_stream_lsl.py
+++ mne_lsl/stream/tests/test_stream_lsl.py
@@ -503,14 +503,14 @@ def test_stream_n_new_samples(mock_lsl_stream, caplog):
     _, _ = stream.get_data()
     # Between the above call and this one, samples could come in...
     # but hopefully not many
-    assert stream.n_new_samples < 100
+    assert stream.n_new_samples < 401  # a chunk is 200 samples
     with _use_log_level("INFO"):
         caplog.set_level(20)  # INFO
         caplog.clear()
         time.sleep(1.6)
         assert "new samples exceeds the buffer size" in caplog.text
     _, _ = stream.get_data(winsize=0.1)
-    assert stream.n_new_samples < 100
+    assert stream.n_new_samples < 401  # a chunk is 200 samples
     stream.disconnect()
 
 
