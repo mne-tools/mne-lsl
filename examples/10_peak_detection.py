3e6588a247b69db4f074650d420acfe87727e1b5
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-06-14 13:55:16 +0200

    Use property n_new_samples in detector examlpe (#288)

--- examples/10_peak_detection.py
+++ examples/10_peak_detection.py
@@ -292,7 +292,6 @@ class Detector:
         self._stream.notch_filter(100, picks=ch_name)
         sleep(bufsize)  # prefill an entire buffer
         # peak detection settings
-        self._last_acq_time = None
         self._last_peak = None
         self._peak_candidates = None
         self._peak_candidates_count = None
@@ -305,11 +304,9 @@ class Detector:
         peaks : array of shape (n_peaks,)
             The timestamps of all detected peaks.
         """
-        data, ts = self._stream.get_data()  # we have a single channel in the stream
-        if self._last_acq_time is None or self._last_acq_time != ts[-1]:
-            self._last_acq_time = ts[-1]
-        elif self._last_acq_time == ts[-1]:
+        if self._stream.n_new_samples == 0:
             return np.array([])  # nothing new to do
+        data, ts = self._stream.get_data()  # we have a single channel in the stream
         data = data.squeeze()
         peaks, _ = find_peaks(
             data,
