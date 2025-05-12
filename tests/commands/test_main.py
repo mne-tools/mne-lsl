1dfc70a40e72d30c11f9846f41205c312915a909
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2025-05-12 15:56:26 +0200

    test fix for click>=8.2.0

--- tests/commands/test_main.py
+++ tests/commands/test_main.py
@@ -8,7 +8,7 @@ from mne_lsl.commands.main import run
 def test_main() -> None:
     """Test the main package entry-point."""
     runner = CliRunner()
-    result = runner.invoke(run)
+    result = runner.invoke(run, ["--help"])
     assert result.exit_code == 0
     assert "Options:" in result.output
     assert "Commands:" in result.output
