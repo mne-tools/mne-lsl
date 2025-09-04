f52fc382de2a6a84a3b6259156d1591bdadb1bc8
Mathieu Scheltienne <mathieu.scheltienne@dandelion.science>
2025-09-04 14:38:25 +0200

    fixes

--- tests/_commands/test_sys_info.py
+++ tests/_commands/test_sys_info.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import pytest
 from click.testing import CliRunner
 
-from mne_lsl.commands.sys_info import run
+from mne_lsl._commands.sys_info import run
 
 
 @pytest.mark.parametrize("developer", [False, True])
